9a6a8fc9ca5ad3179d5bb1940ecbcc3be1df9266
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2022-07-06 15:59:14 +0100

    fixed a minizer bug

--- src/enanoscopy/methods/image/drift/estimate_shift.py
+++ src/enanoscopy/methods/image/drift/estimate_shift.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.interpolate import griddata
 from scipy.optimize import minimize
-from scipy.interpolate import interp2d
 
 
 # TODO refactor to not create a new griddata object on every iteration
@@ -11,9 +10,6 @@ class GetMaxOptimizer(object):
         self.slice_ccm = slice_ccm
         self.w = slice_ccm.shape[1]
         self.h = slice_ccm.shape[0]
-        self.interpolator = interp2d(np.array(range(self.slice_ccm.shape[0])),
-                                     np.array(self.slice_ccm.shape[1]),
-                                     self.slice_ccm.reshape((self.slice_ccm.shape[0] * self.slice_ccm.shape[1])))
         
     def get_interpolated_px_value(self, coords):
         points = [(y, x) for y in range(self.slice_ccm.shape[0]) for x in range(self.slice_ccm.shape[1])]
@@ -21,11 +17,8 @@ class GetMaxOptimizer(object):
 
         return -griddata(points, values, coords, method="cubic")
 
-    def get_interpolated_px_value_optimized(self, coords):
-        return -self.interpolator(coords[0], coords[1])
-
     def get_max(self):
         y_max, x_max = np.unravel_index(self.slice_ccm.argmax(), self.slice_ccm.shape)
-        minimizer = minimize(self.get_interpolated_px_value, np.array([y_max, x_max]), method="Nelder-Mead", options={"maxiter": 1000})
+        minimizer = minimize(self.get_interpolated_px_value, (y_max, x_max), method="Nelder-Mead", options={"maxiter": 1000})
         return minimizer.x
 
