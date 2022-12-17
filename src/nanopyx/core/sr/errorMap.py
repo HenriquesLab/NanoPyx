606d2d99a3924e33b47a46afc695d87db7a5c977
Ricardo Henriques <paxcalpt@gmail.com>
2022-12-17 23:19:31 +0000

    Update errorMap.py
    code cleanup

--- src/nanopyx/core/sr/errorMap.py
+++ src/nanopyx/core/sr/errorMap.py
@@ -2,36 +2,48 @@
 
 import numpy as np
 from scipy.ndimage import gaussian_filter
-from scipy.optimize import brent # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.brent.html
-from scipy.stats import linregress # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html
+from scipy.optimize import (
+    brent,  # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.brent.html
+)
+from scipy.stats import (
+    linregress,  # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.linregress.html
+)
+from skimage.transform import resize
 
-from nanopyx.core.analysis.pearsonCorrelation import pearsonCorrelation
+from ..analysis.pearsonCorrelation import pearsonCorrelation
+
 
 class ErrorMap:
-    
     def __init__(self):
         self.vRSE: float = 0
         self.vRSP: float = 0
         self.alpha: float = 0
         self.beta: float = 0
         self.sigma: float = 0
-        
+
         self.imRef: np.ndarray = None
         self.imSR: np.ndarray = None
         self.imSRIntensityScaledBlurred: np.ndarray = None
         self.imRSE: np.ndarray = None
-        
-    def optimise(self, imRef: np.ndarray, imSR: np.ndarray, fixedSigma = 0):
+
+    def optimise(self, imRef: np.ndarray, imSR: np.ndarray, fixedSigma=0):
         self.imRef = imRef
         self.imSR = imSR
-        
+
         magnification = imRef.shape[0] / imSR.shape[0]
         assert magnification == imRef.shape[1] / imSR.shape[1]
-        maxSigmaBoundary = (4 / 2.35482) * magnification # this assumes Nyquist sampling in the ref image
-        
+        maxSigmaBoundary = (
+            4 / 2.35482
+        ) * magnification  # this assumes Nyquist sampling in the ref image
+
         sigma_linear = fixedSigma * magnification
         if fixedSigma == 0:
-            sigma_linear = brent(sigmaFunction2Optimize, args=(imRef, imSR), brack=(0, maxSigmaBoundary), maxiter=1000)
+            sigma_linear = brent(
+                sigmaFunction2Optimize,
+                args=(imRef, imSR),
+                brack=(0, maxSigmaBoundary),
+                maxiter=1000,
+            )
 
         if abs(sigma_linear - maxSigmaBoundary) < 0.0001:
             print("RSF constrained, as no good minimum found")
@@ -41,25 +53,38 @@ class ErrorMap:
         self.alpha = alpha
         self.beta = beta
         self.sigma = sigma_linear
-        self.imSRIntensityScaledBlurred = gaussian_filter(imSR * self.alpha + self.beta, self.sigma)
-        self.imRSE = np.abs(self.imSRIntensityScaledBlurred-self.imRef)
-        self.vRSE = np.mean((self.imSRIntensityScaledBlurred - self.imRef)**2)**0.5
+        self.imSRIntensityScaledBlurred = gaussian_filter(
+            imSR * self.alpha + self.beta, self.sigma
+        )
+        self.imRSE = np.abs(self.imSRIntensityScaledBlurred - self.imRef)
+        self.vRSE = np.mean((self.imSRIntensityScaledBlurred - self.imRef) ** 2) ** 0.5
         self.vRSP = pearsonCorrelation(self.imSRIntensityScaledBlurred, self.imRef)
 
-    def getRSE(self):
+    def getRSE(self) -> float:
         return self.vRSE
 
-    def getRSP(self):
+    def getRSP(self) -> float:
         return self.vRSP
 
-def calculateAlphaBeta(sigma: float, imRef: np.ndarray, imSR: np.ndarray):
+
+def calculateAlphaBeta(sigma: float, imRef: np.ndarray, imSR: np.ndarray) -> tuple[float, float]:
+    """Gaussian blurs imSR image and calculates linear regressino again imRef
+
+    Args:
+        sigma (float): gaussian blur sigma
+        imRef (np.ndarray): reference image (generally a difraction limited equivalent)
+        imSR (np.ndarray): super-resolution image
+
+    Returns:
+        tuple[float, float]: alpha and beta for linear regression
+    """
     imSRBlurred = gaussian_filter(imSR, sigma)
     slope, intercept, r, p, se = linregress(imSRBlurred.ravel(), imRef.ravel())
     return slope, intercept
 
+
 def sigmaFunction2Optimize(sigma: float, imRef: np.ndarray, imSR: np.ndarray) -> float:
     alpha, beta = calculateAlphaBeta(sigma, imRef, imSR)
     imSRIntensityScaledBlurred = gaussian_filter(imSR * alpha + beta, sigma)
-    rmse = np.mean((imSRIntensityScaledBlurred - imRef)**2)**0.5
+    rmse = np.mean((imSRIntensityScaledBlurred - imRef) ** 2) ** 0.5
     return rmse
-
