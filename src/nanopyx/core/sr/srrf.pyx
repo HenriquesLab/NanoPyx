562c1dadb226ec1b0ddd0ef3b2db6ca35fff7469
Ricardo Henriques <paxcalpt@gmail.com>
2022-12-26 12:15:31 +0000

    small improvements to srrf

--- src/nanopyx/core/sr/srrf.pyx
+++ src/nanopyx/core/sr/srrf.pyx
@@ -1,6 +1,6 @@
-# cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3
+# cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3, profile=True
 
-from libc.math cimport pow, log, sqrt, exp, pi, floor, fabs, fmax, fmin, cos, sin
+from libc.math cimport sqrt, pi, floor, fabs, cos, sin
 
 import numpy as np
 cimport numpy as np
@@ -9,35 +9,28 @@ from cython.parallel import prange
 
 # code based on https://github.com/HenriquesLab/NanoJ-SRRF/blob/master/SRRF/src/nanoj/srrf/java/SRRF.java
 
-cdef class SRRF:
-    cdef int magnification, order, symmetryAxis, border, nRingCoordinates
-    cdef float spatialRadius, psfWidth, gradRadius
+cdef class CalculateRadiality:
+    cdef int magnification, symmetryAxis, border, nRingCoordinates
+    cdef float ringRadius, psfWidth, gradRadius
     cdef bint doIntegrateLagTimes, radialityPositivityConstraint, doIntensityWeighting
+    cdef float[12] xRingCoordinates, yRingCoordinates
 
-    cdef float[:] xRingCoordinates, yRingCoordinates
-
-    def __init__(self, int magnification, int order, int symmetryAxis, float spatialRadius, int border, 
-        bint doIntegrateLagTimes, bint radialityPositivityConstraint, bint doIntensityWeighting):
+    def __init__(self, int magnification, float ringRadius, int border, bint radialityPositivityConstraint, bint doIntensityWeighting):
         self.magnification = magnification
         self.border = border
-        self.symmetryAxis = symmetryAxis
-        self.spatialRadius = spatialRadius
+        self.ringRadius = ringRadius * magnification
         self.border = border
-        self.doIntegrateLagTimes = doIntegrateLagTimes
         self.radialityPositivityConstraint = radialityPositivityConstraint
         self.doIntensityWeighting = doIntensityWeighting
+        self.nRingCoordinates = 12
 
-        self.nRingCoordinates = symmetryAxis * 2
-
-        self.xRingCoordinates = np.empty(self.nRingCoordinates, dtype=np.float32) # new float[nRingCoordinates];
-        self.yRingCoordinates = np.empty(self.nRingCoordinates, dtype=np.float32)
         cdef float angleStep = (pi * 2.) / self.nRingCoordinates
         with nogil:
             for angleIter in range(self.nRingCoordinates):
-                self.xRingCoordinates[angleIter] = cos(angleStep * angleIter)
-                self.yRingCoordinates[angleIter] = sin(angleStep * angleIter)
+                self.xRingCoordinates[angleIter] = cos(angleStep * angleIter) * self.ringRadius
+                self.yRingCoordinates[angleIter] = sin(angleStep * angleIter) * self.ringRadius
 
-    def calculateRadiality(self, im: np.ndarray):
+    def calculate(self, im: np.ndarray):
         assert im.ndim == 3
 
         nFrames = im.shape[0]
@@ -48,8 +41,9 @@ cdef class SRRF:
         cdef float [:,:,:] imRad = np.zeros((im.shape[0], im.shape[1]*self.magnification, im.shape[2]*self.magnification), dtype=np.float32)
         cdef float [:,:,:] imIW = np.zeros((im.shape[0], im.shape[1]*self.magnification, im.shape[2]*self.magnification), dtype=np.float32)
 
+        cdef int n
         with nogil:
-            for n in range(nFrames):
+            for n in prange(nFrames):
                 self._calculate_radiality(imRaw[n,:,:], imRad[n,:,:], imIW[n,:,:], imGx[n,:,:], imGy[n,:,:], 0, 0)
 
         return imRad, imIW, imGx, imGy
@@ -62,23 +56,22 @@ cdef class SRRF:
         cdef int w = imRaw.shape[0]
         cdef int h = imRaw.shape[1]
         cdef int i, j, sampleIter
-        cdef float x0, y0, xc, yc, GMag, GdotR, xRing, yRing
+        cdef float x0, y0, xc, yc, GMag, xRing, yRing
 
         # calculate Gx and Gy
-        cdef float vGx, vGy, GMagRadius
+        cdef float vGx, vGy
         cdef float CGH # for Culley Gustafsson Henriques transform 
 
         # Radiality Variable
         cdef float Dk, DivDFactor = 0
-        cdef int isDiverging = -1
  
         for j in range(1, h-1):
             for i in range(1, w-1):
                 imGx[i,j] = -imRaw[i-1,j]+imRaw[i+1,j]
                 imGy[i,j] = -imRaw[i,j-1]+imRaw[i,j+1]
 
-        for j in range(1 * self.magnification, (h - 1) * self.magnification):
-            for i in range(1 * self.magnification, (w - 1) * self.magnification):
+        for j in range((1 + self.border) * self.magnification, (h - 1 - self.border) * self.magnification):
+            for i in range((1 + self.border) * self.magnification, (w - 1 - self.border) * self.magnification):
                 xc = i + 0.5 + shiftX * self.magnification
                 yc = j + 0.5 + shiftY * self.magnification
 
@@ -87,8 +80,8 @@ cdef class SRRF:
                 # Output
                 CGH = 0
                 for sampleIter in range(0, self.nRingCoordinates):
-                    xRing = self.xRingCoordinates[sampleIter] * self.spatialRadius
-                    yRing = self.yRingCoordinates[sampleIter] * self.spatialRadius
+                    xRing = self.xRingCoordinates[sampleIter]
+                    yRing = self.yRingCoordinates[sampleIter]
 
                     x0 = xc + xRing
                     y0 = yc + yRing
@@ -96,12 +89,11 @@ cdef class SRRF:
                     vGx = _interpolate(imGx, x0, y0, self.magnification)
                     vGy = _interpolate(imGy, x0, y0, self.magnification)
                     GMag = sqrt(vGx * vGx + vGy * vGy)
-                    GMagRadius = GMag * self.spatialRadius
 
-                    Dk = 1 - self.calculateDk(x0, y0, xc, yc, vGx, vGy, GMag) / self.spatialRadius
+                    Dk = 1 - self._calculateDk(x0, y0, xc, yc, vGx, vGy, GMag) / self.ringRadius
                     Dk = Dk * Dk
                     
-                    if (vGx * xRing + vGy * yRing) > 0:
+                    if (vGx * xRing + vGy * yRing) > 0: # inwards or outwards vector
                         DivDFactor -= Dk
                     else:
                         DivDFactor += Dk
@@ -120,13 +112,13 @@ cdef class SRRF:
                     imRad[i,j] = CGH * imIW[i,j]
 
                 
-    cdef float calculateDk(self, float x, float y, float xc, float yc, float vGx, float vGy, float vGx2Gy2) nogil:
+    cdef float _calculateDk(self, float x, float y, float xc, float yc, float vGx, float vGy, float vGx2Gy2) nogil:
         if vGx2Gy2 == 0:
-            return self.spatialRadius
+            return self.ringRadius
         else:
             return fabs(vGy * (xc - x) - vGx * (yc - y)) / vGx2Gy2
 
-cdef float cubic(float x) nogil:
+cdef float _cubic(float x) nogil:
     cdef float a = 0.5  # Catmull-Rom interpolation
     cdef float z = 0
     if x < 0: 
@@ -138,6 +130,9 @@ cdef float cubic(float x) nogil:
     return z
 
 cdef float _interpolate(float[:,:] im, float x, float y, int magnification) nogil:
+    """
+    Carryout Catmull-Rom interpolation
+    """
     cdef int w = im.shape[0]
     cdef int h = im.shape[1]
     x = x / magnification
@@ -157,7 +152,7 @@ cdef float _interpolate(float[:,:] im, float x, float y, int magnification) nogi
         p = 0
         for i in range(4):
             u = u0 - 1 + i
-            p = p + im[u, v] * cubic(x - (u + 0.5))
-        q = q + p * cubic(y - (v + 0.5))
+            p = p + im[u, v] * _cubic(x - (u + 0.5))
+        q = q + p * _cubic(y - (v + 0.5))
 
     return q
