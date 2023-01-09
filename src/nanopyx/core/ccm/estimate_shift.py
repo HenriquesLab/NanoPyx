421a019d326173535edf8c08cf8f96ef1a136d0b
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-01-09 14:05:01 +0000

    removed unused interpolation method

--- src/nanopyx/core/ccm/estimate_shift.py
+++ src/nanopyx/core/ccm/estimate_shift.py
@@ -11,12 +11,6 @@ class GetMaxOptimizer(object):
         self.w = slice_ccm.shape[1]
         self.h = slice_ccm.shape[0]
         self.interpolator = interp2d([y for y in range(self.h)], [x for x in range(self.w)], self.slice_ccm.reshape((self.w * self.h)), kind="cubic")
-        
-    def get_interpolated_px_value(self, coords):
-        points = [(y, x) for y in range(self.slice_ccm.shape[0]) for x in range(self.slice_ccm.shape[1])]
-        values = self.slice_ccm.reshape((self.slice_ccm.shape[0] * self.slice_ccm.shape[1]))
-
-        return -griddata(points, values, coords, method="cubic")
 
     def get_interpolated_px_value_interp2d(self, coords):
         return -self.interpolator(coords[0], coords[1])
