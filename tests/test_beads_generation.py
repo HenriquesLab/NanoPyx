4217e3a586a4305c0f4ae6e88bb13d6a02ab62cf
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-01-10 17:38:52 +0000

    added testing for bead generation with random drift

---
+++ tests/test_beads_generation.py
@@ -0,0 +1,7 @@
+from nanopyx.core.utils.imagegenerator.beads import generate_timelapse_drift
+
+
+def test_generate_random_drift():
+    img = generate_timelapse_drift(shape=(10, 300, 300), drift=None, drift_mode="random")
+
+    assert img.shape == (10, 300, 300)
