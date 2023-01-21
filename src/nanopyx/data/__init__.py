92d84cd63b8168d0978abf5e3d9363470f39ce28
Ricardo Henriques <paxcalpt@gmail.com>
2023-01-21 18:53:18 +0000

    Update __init__.py
    Small bug fix

--- src/nanopyx/data/__init__.py
+++ src/nanopyx/data/__init__.py
@@ -5,17 +5,16 @@ Contains helper methods for downloading and loading example test data
 >>> downloader = ExampleDataManager()
 >>> datasets = downloader.list_datasets()
 >>> print("\\n".join([" - "+dataset for dataset in datasets]))
+ - EMBL_TubulinGFP
+ - ImmunoSynapseFormation_LifeActGFP
  - LongContinuous_UtrGFP
  - PumpyCost7_UtrGFP
  - SMLMS2013_HDTubulinAlexa647
  - SM_U2OS_Pereira_MTAlexaA647_V1
- - ShortContinuous_UtrGFP
- - EMBL_TubulinGFP
- - ImmunoSynapseFormation_LifeActGFP
  - SM_U2OS_Pereira_MTAlexaA647_V2
+ - ShortContinuous_UtrGFP
 >>> thumbnail_path = downloader.get_thumbnail("LongContinuous_UtrGFP")
 >>> import os
 >>> "/".join(["..."]+thumbnail_path.split(os.path.sep)[-5:])
 '.../nanopyx/data/examples/LongContinuous_UtrGFP/thumbnail.jpg'
-    
 """
