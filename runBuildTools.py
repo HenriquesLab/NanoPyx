c78497ec61a2d63481a52664e88d8c6d59aaa685
antmsbrito <50997716+antmsbrito@users.noreply.github.com>
2023-01-24 10:17:20 +0000

    Fixing runBuildTools.py for windows

--- runBuildTools.py
+++ runBuildTools.py
@@ -87,13 +87,13 @@ def main(mode = None):
         else "echo 'No files to clean'",
         "Clear notebook output": f"jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {notebook_files}",
         "Update .gitignore": update_gitignore,
-        "Run pdoc": "python -m pdoc src/nanopyx -o docs",
+        "Run pdoc": f"{python_call} -m pdoc src/nanopyx -o docs",
         "Install nanopyx in developer mode": "pip3 install -e '.[jupyter,test,doc,developer]'",
-        "Build nanopyx binary distribution": "python3 setup.py bdist_wheel",
-        "Build nanopyx source distribution": "python3 setup.py sdist",
-        "Install coding tools": "pip install cython-lint",
-        
+        "Build nanopyx binary distribution": f"{python_call} setup.py bdist_wheel",
+        "Build nanopyx source distribution": f"{python_call} setup.py sdist",
+        "Install coding tools": "pip3 install cython-lint",
         "Run cython-lint on pyx files": f"cython-lint {', '.join(find_files('src', '.pyx'))}",
+
         "Create venv:": "python3 -m venv .venv",
         "Activate venv:": "source .venv/bin/activate",
         "Deactivate venv:": "deactivate",
