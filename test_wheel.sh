1a8f86044b05155572c549dee7b1cbc230bcdf13
Ricardo Henriques <paxcalpt@gmail.com>
2023-02-09 12:01:20 +0000

    Delete test_wheel.sh

--- test_wheel.sh
+++
@@ -1,8 +0,0 @@
-python3.11 setup.py bdist_wheel
-rm -rf .venv-test
-python3.11 -m venv .venv-test
-source .venv-test/bin/activate
-.venv-test/bin/pip install --upgrade pip
-.venv-test/bin/pip install 'dist/nanopyx-0.0.1-cp311-cp311-macosx_10_9_universal2.whl[test]'
-cd ..
-NanoPyx/.venv-test/bin/pytest NanoPyx/tests
