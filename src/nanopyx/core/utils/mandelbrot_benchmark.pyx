880ac06c5a8e03842e72aff6e21fcef62f3641a3
Ricardo Henriques <paxcalpt@gmail.com>
2023-02-10 11:06:41 +0000

    fixes incorrect benchmark

--- src/nanopyx/core/utils/mandelbrot_benchmark.pyx
+++ src/nanopyx/core/utils/mandelbrot_benchmark.pyx
@@ -4,7 +4,8 @@ from cython.parallel import prange
 import numpy as np
 import time
 
-cdef int mandelbrot(int x, int y, int max_iter) nogil:
+
+cdef int mandelbrot(double x, double y, int max_iter, double divergence) nogil:
     cdef double real, imag
     cdef double real2, imag2
     cdef int i
@@ -13,41 +14,49 @@ cdef int mandelbrot(int x, int y, int max_iter) nogil:
     real2, imag2 = real, imag
     for i in range(max_iter):
         real2, imag2 = real2 * real2 - imag2 * imag2 + real, 2 * real2 * imag2 + imag
-        if real2 * real2 + imag2 * imag2 > 4:
+        if real2 * real2 + imag2 * imag2 > divergence:
             return i
     return max_iter
 
-def create_fractal_cython_threaded(int size, int max_iter):
+
+def create_fractal_cython_threaded(int size, int max_iter, divergence: float=4):
     """
     Create a fractal image using the mandelbrot algorithm
     using a threaded cython version (with openmp)
     :param size: size of the image
     :param max_iter: maximum number of iterations
+    :param divergence: divergence threshold
     """
     cdef int[:, ::1] image = np.zeros((size, size), dtype=np.int32)
     cdef int i, j
+    cdef float _divergence = divergence
+
     with nogil:
         for i in prange(size):
             for j in range(size):
-                image[i, j] = mandelbrot(j, i, max_iter)
+                image[i, j] = mandelbrot(j, i, max_iter, _divergence)
     return image
 
-def create_fractal_cython_nonthreaded(int size, int max_iter):
+
+def create_fractal_cython_nonthreaded(int size, int max_iter, divergence: float=4):
     """
     Create a fractal image using the mandelbrot algorithm
     using a nonthreaded cython version
     :param size: size of the image
     :param max_iter: maximum number of iterations
+    :divergence: divergence threshold
     """
     cdef int[:, ::1] image = np.zeros((size, size), dtype=np.int32)
     cdef int i, j
+    cdef float _divergence = divergence
     with nogil:
         for i in range(size):
             for j in range(size):
-                image[i, j] = mandelbrot(j, i, max_iter)
+                image[i, j] = mandelbrot(j, i, max_iter, _divergence)
     return image
 
-def check_acceleration(size: int = 128, max_iter: int = 10):
+
+def check_acceleration(size: int = 1000, max_iter: int = 1000, divergence: float = 4):
     """
     Check the acceleration of the Cython code threaded vs non-threaded
     :param size: size of the image
@@ -55,14 +64,15 @@ def check_acceleration(size: int = 128, max_iter: int = 10):
     :return: tuple of the time taken for the threaded and non-threaded version
     """
     start = time.time()
-    create_fractal_cython_threaded(size, max_iter)
+    create_fractal_cython_threaded(size, max_iter, divergence)
     end = time.time()
     delta_threaded = end - start
     print(f"Cython threaded took: {round(delta_threaded*1000., 3)}ms")
     start = time.time()
-    create_fractal_cython_nonthreaded(size, max_iter)
+    create_fractal_cython_nonthreaded(size, max_iter, divergence)
     end = time.time()
     delta_nonthreaded = end - start
     print(f"Cython nonthreaded took {round(delta_nonthreaded*1000., 3)}ms")
-    print(f"Threaded is {round(delta_threaded/delta_nonthreaded, 2)}x faster")
+
+    print(f"Threaded is {round(delta_nonthreaded/delta_threaded, 2)}x faster")
     return delta_threaded, delta_nonthreaded
