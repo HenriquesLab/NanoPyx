55a2ab3e1bf1cceb7c45ab6ae7d5ddd0efe8ca26
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-02-15 17:05:40 +0000

    bugfixing

--- src/nanopyx/core/sr/decorr.py
+++ src/nanopyx/core/sr/decorr.py
@@ -4,8 +4,9 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 import pandas as pd
 
-from .utils import apodize_edges, normalizeFFT, linmap
+from .decorr_utils import *
 from ..analysis.ccm.helper_functions import make_even_square
+from ..utils.time.timeit import timeit2
 
 class DecorrAnalysis(object):
     
@@ -49,30 +50,20 @@ class DecorrAnalysis(object):
         self.c = 1
         self.save_path = save_path
         self.results_table = pd.DataFrame(columns=["Frame", "Channel", "Z", "Resolution", "Units", "A0", "Kc", "Kc GM", "rMin", "rMax", "Nr", "Ng"])
-        
+    
+    def normalizeFFT(self, fft_real, fft_imag):
+        return normalizeFFT(fft_real, fft_imag)
+    
+    def apodize_edges(self, img):
+        return apodize_edges(img)
+    
     def get_mask(self, w, r2):
         
-        r2 = r2*w*w/4
-        mask = np.zeros((w, w), dtype=np.float32)
-        
-        for y_i in range(w):
-            for x_i in range(w):
-                dist = (y_i - w/2)**2 + (x_i - w/2)**2
-                if dist < r2:
-                    mask[y_i, x_i] = 1
-                    
-        return mask
+        return get_mask(w, r2)
     
     def get_corr_coef_norm(self, fft_real, fft_imag, mask):
         
-        c = 0
-        
-        for y_i in range(fft_real.shape[0]):
-            for x_i in range(fft_real.shape[1]):
-                if mask[y_i, x_i] == 1:
-                    c += fft_real[y_i, x_i]**2 + fft_imag[y_i, x_i]**2
-                    
-        return math.sqrt(c)
+        return get_corr_coef_norm(fft_real, fft_imag, mask)
     
     def get_corr_coef_ring(self, fft_real, fft_imag, normalized_fft_real, normalized_fft_imag):
         out = np.empty((2*int(self.n_r)), dtype=np.float32)
@@ -86,7 +77,6 @@ class DecorrAnalysis(object):
         oy = int(height * (1-self.rmax)/2)
         w = int(width * self.rmax)
         h = int(height * self.rmax)
-        
         for x_i in range(ox, ox+w):
             for y_i in range(oy, oy+h):
                 dist = (x_i-width/2)**2 + (y_i-height/2)**2
@@ -101,13 +91,13 @@ class DecorrAnalysis(object):
                             dist = 0
                         d = math.floor(dist)
                         out[d] += fft_real[y_i, x_i] * normalized_fft_real[y_i, x_i] + fft_imag[y_i, x_i] * normalized_fft_imag[y_i, x_i]
-                        out[d+self.n_r] = normalized_fft_real[y_i, x_i]**2 + normalized_fft_imag[y_i, x_i]**2
+                        out[d+self.n_r] += normalized_fft_real[y_i, x_i]**2 + normalized_fft_imag[y_i, x_i]**2
                         
         return out
-        
+    
     def compute_d0(self, fft_real, fft_imag):
         
-        normalized_fft = normalizeFFT(fft_real, fft_imag)
+        normalized_fft = self.normalizeFFT(fft_real, fft_imag)
         normalized_fft_real = normalized_fft[0]
         normalized_fft_imag = normalized_fft[1]
 
@@ -129,32 +119,12 @@ class DecorrAnalysis(object):
             
         if math.isnan(self.d0[0]):
             self.d0[0] = 0
-        
+    
     def get_max(self, arr, x1, x2):
-        out = np.empty((2), dtype=np.float32)
-        
-        out[0] = x1
-        out[1] = arr[x1]
-        
-        for k in range(x1, x2):
-            if arr[k] > out[1]:
-                out[0] = k
-                out[1] = arr[k]
-                
-        return out
-                
+        return get_max(arr, x1, x2)
+    
     def get_min(self, arr, x1, x2):
-        out = np.empty((2), dtype=np.float32)
-        
-        out[0] = x1
-        out[1] = arr[x1]
-        
-        for k in range(x1, x2):
-            if arr[k] < out[1]:
-                out[0] = k
-                out[1] = arr[k]
-                
-        return out
+        return get_min(arr, x1, x2)
     
     def get_d_corr_max(self, d, r1, r2):
         
@@ -185,33 +155,10 @@ class DecorrAnalysis(object):
         return out
     
     def get_best_score(self, kc, a):
-        gm = np.empty(self.kc.shape, dtype=np.float32)
-        out = np.empty((3), dtype=np.float32)
-        gm_max = 0
-        
-        for k in range(kc.shape[0]):
-            gm[k] = kc[k]*a[k]
-            
-            if gm[k] > gm_max:
-                gm_max = gm[k]
-                out[0] = kc[k]
-                out[1] = a[k]
-                out[2] = k
-                
-        return out
+        return get_best_score(kc, a)
     
     def get_max_score(self, kc, a):
-        out = np.empty((3), dtype=np.float32)
-        kc_max = 0
-        
-        for k in range(kc.shape[0]):
-            if kc[k] > kc_max:
-                kcmax = kc[k]
-                out[0] = kc[k]
-                out[1] = a[k]
-                out[2] = k
-                
-        return out
+        return get_max_score(kc, a)
     
     def compute_d(self):
         
@@ -226,7 +173,7 @@ class DecorrAnalysis(object):
         
         ref = np.array([self.img_ref.copy()], dtype=np.float32)
         
-        ref = make_even_square(ref)[0]
+        ref = np.array(make_even_square(ref)[0], dtype=np.float32)
         
         crmin = self.rmin
         crmax = self.rmax
@@ -243,10 +190,9 @@ class DecorrAnalysis(object):
                 fft = np.fft.fft2(blurred)
                 fft_real = fft.real.astype(np.float32)
                 fft_imag = fft.imag.astype(np.float32)
-                normalized_fft = normalizeFFT(fft_real, fft_imag)
+                normalized_fft = self.normalizeFFT(fft_real, fft_imag)
                 normalized_fft_real = normalized_fft[0]
                 normalized_fft_imag = normalized_fft[1]
-                
                 cr = self.get_corr_coef_norm(fft_real, fft_imag, mask)
         
                 coef = self.get_corr_coef_ring(fft_real, fft_imag, normalized_fft_real, normalized_fft_imag)
@@ -264,7 +210,6 @@ class DecorrAnalysis(object):
                     
                 if math.isnan(self.d[0][count]):
                     self.d[0][count] = 0
-                    
                 count += 1
                 
             if refine == 0:
@@ -276,13 +221,11 @@ class DecorrAnalysis(object):
                 for j in range(self.n_g):
                     for h in range(self.n_r):
                         dg[h] = self.d[h][j]
-                        
                     result = self.get_d_corr_max(dg, crmin, crmax)
                     kc[self.n_g] = result[0]
                     a[self.n_g] = result[1]
                     self.kc[self.n_g] = result[0]
                     self.a_g[self.n_g] = result[1]
-                    
                 result = self.get_d_corr_max(self.d0, crmin, crmax)
                 kc[self.n_g] = result[0]
                 a[self.n_g] = result[1]
@@ -320,7 +263,6 @@ class DecorrAnalysis(object):
                 a = np.empty((self.n_g), dtype=np.float32)
                 dg = np.empty((self.n_r), dtype=np.float32)
                 result = np.empty((2), dtype=np.float32)
-                
                 for j in range(self.n_g):
                     for h in range(self.n_r):
                         dg[h] = self.d[h][j+self.n_g]
@@ -330,14 +272,57 @@ class DecorrAnalysis(object):
                     a[j] = result[1]
                     self.kc[j+self.n_g] = result[0]
                     self.a_g[j+self.n_g] = result[1]
-                    
                 results_gm = self.get_best_score(self.kc, self.a_g)
                 self.kc_gm = results_gm[0]
                 self.agm = results_gm[1]
                 results_max = self.get_max_score(self.kc, self.a_g)
                 self.kc_max = results_max[0]
                 self.a_max = results_max[1]
+
+    def check_even_square(self, image_arr):
+        w = image_arr.shape[1]
+        h = image_arr.shape[0]
+
+        if w != h:
+            return False
+        if w % 2 != 0:
+            return False
+
+        return True
     
+    def get_closest_even_square(self, image_arr):
+        w = image_arr.shape[1]
+        h = image_arr.shape[0]
+        min_size = min(w, h)
+
+        if min_size % 2 != 0:
+            min_size -= 1
+
+        return min_size
+
+    def make_even_square(self, image_arr):
+        if self.check_even_square(image_arr):
+            return image_arr
+
+        w = image_arr.shape[2]
+        h = image_arr.shape[1]
+        min_size = self.get_closest_even_square_size(image_arr)
+
+        h_start = (h-min_size)//2
+        if (h-min_size) % 2 != 0:
+            h_finish = h - (h-min_size) // 2 - 1
+        else:
+            h_finish = h - (h-min_size) // 2
+
+        w_start = int((w-min_size)/2)
+        if (w - min_size) % 2 != 0:
+            w_finish = w - (w-min_size) // 2 - 1
+        else:
+            w_finish = w - (w-min_size) // 2 
+
+        return image_arr[:, h_start:h_finish, w_start:w_finish]
+    
+    @timeit2
     def run_analysis(self):
         
         for f_i  in range(self.img.shape[0]):
@@ -346,6 +331,17 @@ class DecorrAnalysis(object):
                     self.f = f_i
                     self.c = c_i
                     self.s = s_i
+                    self.d0 = np.empty((self.n_r), dtype=np.float32)
+                    self.d = np.empty((self.n_r, 2*self.n_g), dtype=np.float32)
+                    self.kc = np.empty((2*self.n_g), dtype=np.float32)
+                    self.a_g = np.empty((2*self.n_g), dtype=np.float32)
+                    self.kc0 = 0
+                    self.a0 = 0
+                    self.kc_gm = 0
+                    self.agm = 0
+                    self.kc_max = 0
+                    self.a_max = 0
+                    self.img_ref = None
                     
                     img_ref = self.img[f_i, c_i, s_i].astype(np.float32)
                     
@@ -353,7 +349,8 @@ class DecorrAnalysis(object):
                         img_ref = img_slice[y0:y1, x0:x1]
                         
                     img_f = img_ref.copy()
-                    img_f = np.array([apodize_edges(img_f)])
+                    img_f = np.array([self.apodize_edges(img_f)])
+                    #img_f = img_f.reshape((1, img_f.shape[0], img_f.shape[1]))
                     
                     temp = make_even_square(img_f)[0]
                     self.img_ref = temp.copy()
@@ -372,7 +369,7 @@ class DecorrAnalysis(object):
                     
                     self.compute_d()
                     
-                    self.resolution = int(2*self.pixel_size/self.kc_max)
+                    self.resolution = 2*self.pixel_size/self.kc_max
                     print(f"Resolution: {self.resolution}")
                     
                     self.results_table.loc[len(self.results_table)] = [f_i, c_i, s_i, self.resolution, self.units, self.a0, self.kc_max, self.kc_gm, self.rmin, self.rmax, self.n_r, self.n_g]
