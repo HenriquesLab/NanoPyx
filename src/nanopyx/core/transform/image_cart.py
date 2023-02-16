6e3ddd10d0de1d9d7fbe5e7eaca353bf953e98dd
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-02-16 14:21:49 +0000

    Starting tests for polar and logpolar transforms

--- src/nanopyx/core/transform/image_cart.py
+++ src/nanopyx/core/transform/image_cart.py
@@ -14,33 +14,33 @@ from .interpolation import (
 )
 
 @timeit2
-def catmull_rom_cart(image: np.ndarray, x_shape: int, y_shape: int):
+def catmull_rom_cart(image: np.ndarray, x_shape: int, y_shape: int, scale:str='linear'):
     interpolator = catmull_rom.Interpolator(image)
-    return interpolator.cartesian(x_shape,y_shape)
+    return interpolator.cartesian(x_shape,y_shape,scale)
 
 
 @timeit2
-def lanczos_cart(image: np.ndarray, x_shape: int, y_shape: int, taps: int = 3):
+def lanczos_cart(image: np.ndarray, x_shape: int, y_shape: int, scale:str='linear', taps: int = 3):
     interpolator = lanczos.Interpolator(image, taps)
-    return interpolator.cartesian(x_shape,y_shape)
+    return interpolator.cartesian(x_shape,y_shape,scale)
 
 
 @timeit2
-def bicubic_cart(image: np.ndarray, x_shape: int, y_shape: int):
+def bicubic_cart(image: np.ndarray, x_shape: int, y_shape: int, scale:str='linear'):
     interpolator = bicubic.Interpolator(image)
-    return interpolator.cartesian(x_shape,y_shape)
+    return interpolator.cartesian(x_shape,y_shape,scale)
 
 
 @timeit2
-def bilinear_cart(image: np.ndarray, x_shape: int, y_shape: int):
+def bilinear_cart(image: np.ndarray, x_shape: int, y_shape: int, scale:str='linear'):
     interpolator = bilinear.Interpolator(image)
-    return interpolator.cartesian(x_shape,y_shape)
+    return interpolator.cartesian(x_shape,y_shape,scale)
 
 
 @timeit2
-def nearest_neighbor_cart(image: np.ndarray, x_shape: int, y_shape: int):
+def nearest_neighbor_cart(image: np.ndarray, x_shape: int, y_shape: int, scale:str='linear'):
     interpolator = nearest_neighbor.Interpolator(image)
-    return interpolator.cartesian(x_shape,y_shape)
+    return interpolator.cartesian(x_shape,y_shape,scale)
 
 
 
--- src/nanopyx/core/transform/image_polar.py
+++ src/nanopyx/core/transform/image_polar.py
@@ -16,37 +16,37 @@ from .interpolation import (
 from skimage.transform import warp_polar
 
 @timeit2
-def catmull_rom_polar(image: np.ndarray):
+def catmull_rom_polar(image: np.ndarray, scale:str='linear'):
     interpolator = catmull_rom.Interpolator(image)
-    return interpolator.polar()
+    return interpolator.polar(scale)
 
 
 @timeit2
-def lanczos_polar(image: np.ndarray, taps: int = 3):
+def lanczos_polar(image: np.ndarray, scale:str='linear', taps: int = 3):
     interpolator = lanczos.Interpolator(image, taps)
-    return interpolator.polar()
+    return interpolator.polar(scale)
 
 
 @timeit2
-def bicubic_polar(image: np.ndarray):
+def bicubic_polar(image: np.ndarray, scale:str='linear'):
     interpolator = bicubic.Interpolator(image)
-    return interpolator.polar()
+    return interpolator.polar(scale)
 
 
 @timeit2
-def bilinear_polar(image: np.ndarray):
+def bilinear_polar(image: np.ndarray, scale:str='linear'):
     interpolator = bilinear.Interpolator(image)
-    return interpolator.polar()
+    return interpolator.polar(scale)
 
 
 @timeit2
-def nearest_neighbor_polar(image: np.ndarray):
+def nearest_neighbor_polar(image: np.ndarray, scale:str='linear'):
     interpolator = nearest_neighbor.Interpolator(image)
-    return interpolator.polar()
+    return interpolator.polar(scale)
 
 
 @timeit2
-def skimage_polar(image: np.ndarray):
-    return warp_polar(image)
+def skimage_polar(image: np.ndarray, scale:str='linear'):
+    return warp_polar(image, scaling=scale)
 
 
