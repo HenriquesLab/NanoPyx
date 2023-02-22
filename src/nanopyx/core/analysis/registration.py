f3b281283210bdba0cd9e99ae710a6bae5829dc6
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-02-22 15:52:31 +0000

    Refactored registration class to add more methods

--- src/nanopyx/core/analysis/registration.py
+++ src/nanopyx/core/analysis/registration.py
@@ -19,8 +19,9 @@ class Registration:
 
         self.w = image.shape[1]
         self.h = image.shape[0]
-        self.wref = image.shape[1]
-        self.href = image.shape[0]
+
+        self.wref = ref_image.shape[1]
+        self.href = ref_image.shape[0]
 
 
     def register(self, translation:bool, rotation:bool, scaling:bool):
@@ -31,7 +32,7 @@ class Registration:
         :param scaling: bool defining rotation registration
         """
         
-        if translation and not (rotation and scaling):
+        if translation and (not rotation and not scaling):
             print("Looking for pure translation...", flush=True)
 
             ccm = calculate_ccm_cartesian(self.image, self.ref_image)
@@ -42,23 +43,64 @@ class Registration:
 
             translated = Interpolator(self.image).shift(x_shift,y_shift)
 
+            print((y_shift, x_shift))
+
             return translated
 
-        elif rotation and not (translation and scaling):
+        elif rotation and (not translation and not scaling):
 
             print("Looking for pure rotation...", flush=True)
             
             ccm = calculate_ccm_polar(self.image, self.ref_image)
             shifts = self.calculate_peak(ccm)
 
-            angle = np.deg2rad((180-shifts[0] - 1))
+            angle = -np.deg2rad((180-shifts[0] - 1))
+
+            print(angle)
 
             rotated = Interpolator(self.image).rotate(angle)
 
             return rotated
 
+        elif scaling and (not translation and not rotation):
+
+            print("Looking for pure scaling...", flush=True)
+
+            ccm = calculate_ccm_logpolar(self.image, self.ref_image)
+            shifts = self.calculate_peak(ccm)
+            
+            radius = np.hypot(self.w/2, self.h/2)
+            log_translation = (radius/2 - shifts[1] - 1) * np.log(radius) / radius
+            scaling = np.exp(log_translation)
+            
+            print(scaling)
+
+            scaled = Interpolator(self.image).scale_xy(scaling,scaling)
+
+            return scaled
+        
+        if scaling and rotation and not translation:
+
+            print("Looking for scaling and rotation assuming NO translation...", flush=True)
+
+            ccm = calculate_ccm_logpolar(self.image, self.ref_image)
+            shifts = self.calculate_peak(ccm)
+            
+            radius = np.hypot(self.w/2, self.h/2)
+            log_translation = (radius/2 - shifts[1] - 1) * np.log(radius) / radius
+            scaling = np.exp(log_translation)
+            
+            angle = -np.deg2rad((180-shifts[0] - 1))
+            
+            print(angle, scaling)
+
+            scaled = Interpolator(self.image).scale_xy(scaling,scaling)
+            rotated = Interpolator(scaled).rotate(angle)
+
+            return rotated
+
         else:
-            print("Not implemented", flush=True)
+            print("Looking for rotation, scaling and translation", flush=True)
 
             return None
 
