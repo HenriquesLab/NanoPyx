84036fcd71d6454bb78081d03d1c7d1d9e95d2c3
Inês Cunha <108281775+inesmcunha@users.noreply.github.com>
2023-02-27 18:21:36 +0000

    test for radial gradient convergence

---
+++ tests/test_rgc.py
@@ -0,0 +1,19 @@
+from nanopyx.core.sr.radial_gradient_convergence import RadialGradientConvergence
+from nanopyx.data.download import ExampleDataManager
+from matplotlib import pyplot as plt
+import numpy as np
+
+#downloader = ExampleDataManager()
+
+def test_rgc(downloader):
+
+    dataset = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=True)
+    rgc = RadialGradientConvergence(sensitivity=2)
+
+    imRad, imInt, imGx, imGy, imIntGx, imIntGy = rgc.calculate(dataset)
+    plt.imshow(np.mean(imRad,0), interpolation='none')
+    plt.title('RGC')
+    #plt.show()
+
+#test_rgc(downloader)
