c594a7cc1ec36ef794628205483b78fc0f2749f0
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-03-08 15:20:52 +0000

    added a split frame into blocks function

--- src/nanopyx/core/transform/blocks.pyx
+++ src/nanopyx/core/transform/blocks.pyx
@@ -29,3 +29,28 @@ cdef double[:, :] _assemble_frame_from_blocks(double[:, :, :] blocks_stack, int 
 
     return reconstructed_image
 
+def split_frame_into_blocks(img: np.ndarray, n_rows: int, n_cols: int):
+    return np.array(_split_frame_into_blocks(img.astype(np.float64), n_rows, n_cols), dtype=np.float32)
+
+cdef double[:, :, :] _split_frame_into_blocks(double[:, :] img, int n_rows, int n_cols) nogil:
+    
+        cdef int block_cols_len = img.shape[1] // n_cols
+        cdef int block_rows_len = img.shape[0] // n_rows
+        cdef int cols_len = block_cols_len * n_cols
+        cdef int rows_len = block_rows_len * n_rows
+        cdef int row_i, col_i, count
+    
+        with gil:
+            assert img.shape[0] == rows_len
+            assert img.shape[1] == cols_len
+            blocks_stack = np.empty((n_rows * n_cols, block_rows_len, block_cols_len), dtype=np.float64)
+            assert blocks_stack.shape[0] == n_rows * n_cols
+            assert blocks_stack.shape[1] == block_rows_len
+            assert blocks_stack.shape[2] == block_cols_len
+            count = 0
+            for row_i in range(n_rows):
+                for col_i in range(n_cols):
+                    blocks_stack[count] = img[row_i*block_rows_len:(row_i+1)*block_rows_len, col_i*block_cols_len:(col_i+1)*block_cols_len]
+                    count += 1
+    
+        return blocks_stack
