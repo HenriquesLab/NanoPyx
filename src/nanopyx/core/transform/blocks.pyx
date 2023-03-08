5e881f233a70be6fbea5ce1263dec7097a11a526
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-03-08 14:53:12 +0000

    fixed a bug with new version of blocks

--- src/nanopyx/core/transform/blocks.pyx
+++ src/nanopyx/core/transform/blocks.pyx
@@ -14,19 +14,18 @@ cdef double[:, :] _assemble_frame_from_blocks(double[:, :, :] blocks_stack, int 
     cdef int block_rows_len = blocks_stack.shape[1]
     cdef int cols_len = block_cols_len * n_cols
     cdef int rows_len = block_rows_len * n_rows
-    cdef double[:, :] reconstructed_image
-    cdef int row_i, col_i
+    cdef int row_i, col_i, count
 
     with gil:
         assert blocks_stack.shape[0] == n_rows * n_cols
         reconstructed_image = np.empty((rows_len, cols_len), dtype=np.float64)
         assert reconstructed_image.shape[0] == rows_len
         assert reconstructed_image.shape[1] == cols_len
-
-    for row_i in prange(n_rows):
-        for col_i in range(n_cols):
-            reconstructed_image[row_i * block_rows_len:(row_i + 1) * block_rows_len,
-                                col_i * block_cols_len:(col_i + 1) * block_cols_len] = \
-                                blocks_stack[col_i * n_rows + row_i]
+        count = 0
+        for row_i in range(n_rows):
+            for col_i in range(n_cols):
+                reconstructed_image[row_i*block_rows_len:(row_i+1)*block_rows_len, col_i*block_cols_len:(col_i+1)*block_cols_len] = blocks_stack[count]
+                count += 1
 
     return reconstructed_image
+
