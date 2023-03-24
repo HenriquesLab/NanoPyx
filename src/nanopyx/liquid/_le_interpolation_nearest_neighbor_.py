3bb38cb072cf484fa5c96adb2748d500c1301403
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-03-24 10:29:37 +0000

    Added affine transformation function in python and numba

--- src/nanopyx/liquid/_le_interpolation_nearest_neighbor_.py
+++ src/nanopyx/liquid/_le_interpolation_nearest_neighbor_.py
@@ -105,3 +105,96 @@ def njit_shift_magnify(
                 image_out[f, i, j] = _njit_interpolate(image[f,:,:],row,col,rows,cols)
 
     return image_out
+
+
+
+def shift_scale_rotate(
+    image: np.ndarray,
+    shift_row: np.ndarray,
+    shift_col: np.ndarray,
+    scale_row: float,
+    scale_col: float,
+    angle: float,
+) -> np.ndarray:
+    """
+    Shift, magnify and rotate using nearest neighbor interpolation.
+    :param image: 3D numpy array to interpolate with size (nFrames, nRow, nCol)
+    :param shift_row: 1D array with size (nFrames) with values to shift the rows 
+    :param shift_col: 1D array with size (nFrames) with values to shift the cols
+    :param scale_row: float scale factor for the rows
+    :param scale_col: float scale factor for the cols
+    :param angle: float angle of rotation in radians. positive is counter clockwise
+    :return: 3D float32 numpy array with the result
+    """
+
+    nFrames = image.shape[0]
+    rows = image.shape[1]
+    cols = image.shape[2]
+
+    center_row = rows / 2
+    center_col = cols / 2
+
+    center_rowM = (rows * scale_row) / 2
+    center_colM = (cols * scale_col) / 2
+
+    a = np.cos(angle)/scale_col
+    b = -np.sin(angle)
+    c = np.sin(angle)
+    d = np.cos(angle)/scale_row
+
+    image_out = np.zeros((nFrames, rows, cols), dtype=np.float32)
+
+    for f in range(nFrames):
+        for j in range(cols):
+            for i in range(rows):
+                col = (a*(j-center_colM)+b*(i-center_rowM)) - shift_col[f] + center_col
+                row = (c*(j-center_colM)+d*(i-center_rowM)) - shift_row[f] + center_row
+                image_out[f, i, j] = _interpolate(image[f,:,:],row,col,rows,cols)
+
+    return image_out
+
+@njit(cache=True, parallel=True)
+def njit_shift_scale_rotate(
+    image: np.ndarray,
+    shift_row: np.ndarray,
+    shift_col: np.ndarray,
+    scale_row: float,
+    scale_col: float,
+    angle: float,
+) -> np.ndarray:
+    """
+    Shift, magnify and rotate using nearest neighbor interpolation.
+    :param image: 3D numpy array to interpolate with size (nFrames, nRow, nCol)
+    :param shift_row: 1D array with size (nFrames) with values to shift the rows 
+    :param shift_col: 1D array with size (nFrames) with values to shift the cols
+    :param scale_row: float scale factor for the rows
+    :param scale_col: float scale factor for the cols
+    :param angle: float angle of rotation in radians. positive is counter clockwise
+    :return: 3D float32 numpy array with the result
+    """
+
+    nFrames = image.shape[0]
+    rows = image.shape[1]
+    cols = image.shape[2]
+
+    center_row = rows / 2
+    center_col = cols / 2
+
+    center_rowM = (rows * scale_row) / 2
+    center_colM = (cols * scale_col) / 2
+
+    a = np.cos(angle)/scale_col
+    b = -np.sin(angle)
+    c = np.sin(angle)
+    d = np.cos(angle)/scale_row
+
+    image_out = np.zeros((nFrames, rows, cols), dtype=np.float32)
+
+    for f in range(nFrames):
+        for j in prange(cols):
+            for i in range(rows):
+                col = (a*(j-center_colM)+b*(i-center_rowM)) - shift_col[f] + center_col
+                row = (c*(j-center_colM)+d*(i-center_rowM)) - shift_row[f] + center_row
+                image_out[f, i, j] = _njit_interpolate(image[f,:,:],row,col,rows,cols)
+
+    return image_out
