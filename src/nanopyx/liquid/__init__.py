bdac37a1a421f68076e2e14e342c720c9a5d67de
Ricardo Henriques <paxcalpt@gmail.com>
2023-03-28 10:24:41 +0100

    Update __init__.py

--- src/nanopyx/liquid/__init__.py
+++ src/nanopyx/liquid/__init__.py
@@ -1,5 +1,14 @@
 import os
 import warnings
 
+from .__le_interpolation_catmull_rom import \
+    ShiftAndMagnify as CRShiftAndMagnify
+from .__le_interpolation_catmull_rom import \
+    ShiftScaleRotate as CRShiftScaleRotate
+from .__le_interpolation_nearest_neighbor import \
+    ShiftAndMagnify as NNShiftAndMagnify
+from .__le_interpolation_nearest_neighbor import \
+    ShiftScaleRotate as NNShiftScaleRotate
+from .__le_mandelbrot_benchmark import MandelbrotBenchmark
 from .__njit__ import njit_works
 from .__opencl__ import cl, cl_array, cl_ctx, cl_queue, opencl_works
