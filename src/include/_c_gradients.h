726eea41cb91779f8a072a7964349952793faecf
Inês Cunha <108281775+inesmcunha@users.noreply.github.com>
2023-04-05 15:26:15 +0100

    added 2 point gradient calculation in c

--- src/include/_c_gradients.h
+++ src/include/_c_gradients.h
@@ -4,4 +4,6 @@
 void gradient_roberts_cross(float *image, float *imGr, float *imGc, int rows,
                             int cols);
 
+void gradient_2point(float *image, float *imGx, float *imGy, int h, int w);
+
 #endif
