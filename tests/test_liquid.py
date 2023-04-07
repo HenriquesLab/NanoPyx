e77b8edeebdb15995d1349b191725d60557f45cf
Ricardo Henriques <paxcalpt@gmail.com>
2023-04-07 23:37:35 +0100

    🚀 feat(test_liquid.py): add bicubic interpolation tests This commit adds bicubic interpolation tests to the test_liquid.py file. The bicubic interpolation tests are added for ShiftAndMagnify and ShiftScaleRotate. The tests are added to improve the coverage of the codebase and ensure that the bicubic interpolation functions work as expected.

--- tests/test_liquid.py
+++ tests/test_liquid.py
@@ -1,24 +1,14 @@
 import numpy as np
 
 from nanopyx.core.generate.noise_add_simplex import get_simplex_noise
-from nanopyx.liquid._le_interpolation_catmull_rom import (
-    ShiftAndMagnify as CRShiftAndMagnify,
-)
-from nanopyx.liquid._le_interpolation_catmull_rom import (
-    ShiftScaleRotate as CRShiftScaleRotate,
-)
-from nanopyx.liquid._le_interpolation_lanczos import (
-    ShiftAndMagnify as LZShiftAndMagnify,
-)
-from nanopyx.liquid._le_interpolation_lanczos import (
-    ShiftScaleRotate as LZShiftScaleRotate,
-)
-from nanopyx.liquid._le_interpolation_nearest_neighbor import (
-    ShiftAndMagnify as NNShiftAndMagnify,
-)
-from nanopyx.liquid._le_interpolation_nearest_neighbor import (
-    ShiftScaleRotate as NNShiftScaleRotate,
-)
+from nanopyx.liquid._le_interpolation_bicubic import ShiftAndMagnify as BCShiftAndMagnify
+from nanopyx.liquid._le_interpolation_bicubic import ShiftScaleRotate as BCShiftScaleRotate
+from nanopyx.liquid._le_interpolation_catmull_rom import ShiftAndMagnify as CRShiftAndMagnify
+from nanopyx.liquid._le_interpolation_catmull_rom import ShiftScaleRotate as CRShiftScaleRotate
+from nanopyx.liquid._le_interpolation_lanczos import ShiftAndMagnify as LZShiftAndMagnify
+from nanopyx.liquid._le_interpolation_lanczos import ShiftScaleRotate as LZShiftScaleRotate
+from nanopyx.liquid._le_interpolation_nearest_neighbor import ShiftAndMagnify as NNShiftAndMagnify
+from nanopyx.liquid._le_interpolation_nearest_neighbor import ShiftScaleRotate as NNShiftScaleRotate
 from nanopyx.liquid._le_mandelbrot_benchmark import MandelbrotBenchmark
 
 # flake8: noqa: E501
@@ -58,6 +48,45 @@ def test_interpolation_nearest_neighbor_ShiftAndMagnify(plt):
     shift_row = np.arange(nFrames, dtype=np.float32) * 0.5
     shift_col = np.arange(nFrames, dtype=np.float32) * -0.5
     SM = NNShiftAndMagnify()
+    bench_values = SM.benchmark(image, shift_row, shift_col, M, M)
+
+    images = []
+    titles = []
+    run_times = []
+
+    # unzip the values
+    for run_time, title, image in bench_values:
+        run_times.append(run_time)
+        titles.append(title)
+        images.append(image)
+
+    # ensure images are similar
+    for i in range(len(images)):
+        for j in range(i + 1, len(images)):
+            np.testing.assert_allclose(images[i], images[j], rtol=1e1)
+
+    nFrames = images[0].shape[0]
+    # show images
+    fig, axes = plt.subplots(nFrames, len(images), figsize=(20, 10))
+    for i in range(nFrames):
+        for j in range(len(images)):
+            if i == 0:
+                axes[i, j].set_title(titles[j])
+            axes[i, j].imshow(images[j][i], cmap="hot")
+            axes[i, j].axis("off")
+
+
+# tag-end
+
+
+# tag-copy: test_interpolation_nearest_neighbor_ShiftAndMagnify; replace("nearest_neighbor", "bicubic"); replace("NNShiftAndMagnify", "BCShiftAndMagnify")
+def test_interpolation_bicubic_ShiftAndMagnify(plt):
+    M = 4
+    nFrames = 3
+    image = get_simplex_noise(64, 32, frames=nFrames, amplitude=1000)
+    shift_row = np.arange(nFrames, dtype=np.float32) * 0.5
+    shift_col = np.arange(nFrames, dtype=np.float32) * -0.5
+    SM = BCShiftAndMagnify()
     bench_values = SM.benchmark(image, shift_row, shift_col, M, M)
 
     images = []
@@ -176,6 +205,45 @@ def test_interpolation_nearest_neighbor_ShiftScaleRotate(plt):
     shift_col = np.arange(nFrames, dtype=np.float32) * -0.5
     angle = np.pi / 4
     SM = NNShiftScaleRotate()
+    bench_values = SM.benchmark(image, shift_row, shift_col, M, M, angle)
+
+    images = []
+    titles = []
+    run_times = []
+
+    # unzip the values
+    for run_time, title, image in bench_values:
+        run_times.append(run_time)
+        titles.append(title)
+        images.append(image)
+
+    # ensure images are similar
+    for i in range(len(images)):
+        for j in range(i + 1, len(images)):
+            np.testing.assert_allclose(images[i], images[j], rtol=1e1)
+
+    # show images
+    fig, axes = plt.subplots(nFrames, len(images), figsize=(20, 10))
+    for i in range(nFrames):
+        for j in range(len(images)):
+            if i == 0:
+                axes[i, j].set_title(titles[j])
+            axes[i, j].imshow(images[j][i], cmap="hot")
+            axes[i, j].axis("off")
+
+
+# tag-end
+
+
+# tag-copy: test_interpolation_nearest_neighbor_ShiftScaleRotate; replace("nearest_neighbor", "bicubic"); replace("NNShiftScaleRotate", "BCShiftScaleRotate")
+def test_interpolation_bicubic_ShiftScaleRotate(plt):
+    M = 4
+    nFrames = 3
+    image = get_simplex_noise(64, 32, frames=nFrames, amplitude=1000)
+    shift_row = np.arange(nFrames, dtype=np.float32) * 0.5
+    shift_col = np.arange(nFrames, dtype=np.float32) * -0.5
+    angle = np.pi / 4
+    SM = BCShiftScaleRotate()
     bench_values = SM.benchmark(image, shift_row, shift_col, M, M, angle)
 
     images = []
