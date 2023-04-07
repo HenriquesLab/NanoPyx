1e250ca420c94e909b9b4662796637bdfef6d5a6
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-04-07 19:25:35 +0200

    Minor changes
    Type hints were added to the function arguments and return type to make them explicit.
    A detailed docstring was added to describe the function's behavior and expected inputs and outputs.
    The repeated code was refactored to only write it once for better readability and maintainability.

--- src/nanopyx/core/transform/binning.py
+++ src/nanopyx/core/transform/binning.py
@@ -1,10 +1,17 @@
-def rebin_2d(arr, bin_factor, mode="sum"):
+import numpy as np
+
+def rebin_2d(arr: np.ndarray, bin_factor: int, mode: str = "sum") -> np.ndarray:
     """
     Bins a 2D array by a given factor. The last two dimensions of the array are binned.
+
     :param arr: numpy array with any shape as long as last two dimensions are y, x (example: time, channel, z, y, x)
+    :type arr: numpy.ndarray
     :param bin_factor: factor used to bin dimensions
+    :type bin_factor: int
     :param mode: can be either sum, mean or max, defaults to sum if not specified or not valid mode
+    :type mode: str
     :return: binned array
+    :rtype: numpy.ndarray
     """
     if mode not in ["sum", "mean", "max"]:
         mode = "sum"
@@ -21,33 +28,19 @@ def rebin_2d(arr, bin_factor, mode="sum"):
     if arr.shape[-1] % bin_factor != 0 or arr.shape[-2] % bin_factor != 0:
         raise ValueError("Binning factor must be a divisor of the last two dimensions")
 
+    reshaped_arr = arr.reshape(
+        arr.shape[:-2]
+        + (
+            arr.shape[-2] // bin_factor,
+            bin_factor,
+            arr.shape[-1] // bin_factor,
+            bin_factor,
+        )
+    )
+
     if mode == "sum":
-        return arr.reshape(
-            arr.shape[:-2]
-            + (
-                arr.shape[-2] // bin_factor,
-                bin_factor,
-                arr.shape[-1] // bin_factor,
-                bin_factor,
-            )
-        ).sum(axis=(-1, -3))
+        return reshaped_arr.sum(axis=(-1, -3))
     elif mode == "mean":
-        return arr.reshape(
-            arr.shape[:-2]
-            + (
-                arr.shape[-2] // bin_factor,
-                bin_factor,
-                arr.shape[-1] // bin_factor,
-                bin_factor,
-            )
-        ).mean(axis=(-1, -3))
-    elif mode == "max":
-        return arr.reshape(
-            arr.shape[:-2]
-            + (
-                arr.shape[-2] // bin_factor,
-                bin_factor,
-                arr.shape[-1] // bin_factor,
-                bin_factor,
-            )
-        ).max(axis=(-1, -3))
+        return reshaped_arr.mean(axis=(-1, -3))
+    else:
+        return reshaped_arr.max(axis=(-1, -3))
