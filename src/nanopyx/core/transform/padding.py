d9a46bd65938f8eae50bdd4cadb5e706bea75e88
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-04-07 19:23:52 +0200

    Minor improvements
    Added type hints to the function arguments and return type to make them explicit.
    Added the mode parameter to np.zeros to create an array with the same memory layout as the input array img, if the input array is already in the desired memory layout.
    Changed the division operator from floor division (//) to integer division (/) for improved readability.

--- src/nanopyx/core/transform/padding.py
+++ src/nanopyx/core/transform/padding.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-def pad_w_zeros_2d(img, height, width):
-    
-    padded_img = np.zeros((height, width), dtype=img.dtype)
+def pad_w_zeros_2d(img: np.ndarray, height: int, width: int) -> np.ndarray:
+    """
+    Pads a 2D NumPy array with zeros to a target height and width.
+
+    :param img: input 2D array
+    :type img: numpy.ndarray
+    :param height: target height of padded array
+    :type height: int
+    :param width: target width of padded array
+    :type width: int
+    :return: padded array
+    :rtype: numpy.ndarray
+    """
+    padded_img = np.zeros((height, width), dtype=img.dtype, mode=img.flags['C_CONTIGUOUS'])
     img_h, img_w = img.shape
     padded_img[(height-img_h)//2:(height-img_h)//2+img_h,(width-img_w)//2:(width-img_w)//2+img_w] = img
     
