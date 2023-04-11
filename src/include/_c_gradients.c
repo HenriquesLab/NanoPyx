94e0bdc4e3bdcf2711f04930072e231abd4d39e6
Ricardo Henriques <paxcalpt@gmail.com>
2023-04-11 21:39:37 +0100

    🐛 fix(_c_gradients.c): fix function name to match convention ✨ feat(_c_gradients.c): add new function to calculate radiality gradient ✨ feat(_c_gradients.h): add new function declaration for radiality gradient ✨ feat(sr_radiality.pxd): add new function declaration for radiality gradient 🚀 chore(sr_radiality.pyx): use new radiality gradient function The function name for the 2-point gradient calculation has been changed to match the naming convention. A new function has been added to calculate the radiality gradient. The function declaration has been added to the header file and the pxd file. The new function has been used in the sr_radiality.pyx file to calculate the gradient.

--- src/include/_c_gradients.c
+++ src/include/_c_gradients.c
@@ -1,8 +1,33 @@
 
+void _c_gradient_radiality(float* image, float* imGc, float* imGr, int rows,
+                          int cols) {
+  int c0, c_m1, c_p1, r0, r_m1, r_p1;
+
+  // for j in range(1, h-1):
+  //   for i in range(1, w-1):
+  //       imGx[j,i] = -imRaw[j,i-1]+imRaw[j,i+1]
+  //       imGy[j,i] = -imRaw[j-1,i]+imRaw[j+1,i]
+
+  for (int j = 1; j < rows - 1; j++) {
+    r0 = j * cols;
+    r_m1 = (j - 1) * cols;
+    r_p1 = (j + 1) * cols;
+
+    for (int i = 1; i < cols - 1; i++) {
+      c0 = i;
+      c_m1 = i - 1;
+      c_p1 = i + 1;
+      imGc[r0 + c0] = -image[r0 + c_m1] + image[r0 + c_p1];
+      imGr[r0 + c0] = -image[r_m1 + c0] + image[r_p1 + c0];
+    }
+  }
+}
+
 // as in REF:
 // https://github.com/HenriquesLab/NanoJ-eSRRF/blob/785c71b3bd508c938f63bb780cba47b0f1a5b2a7/resources/liveSRRF.cl
 // under calculateGradient_2point
-void gradient_2point(float *image, float *imGc, float *imGr, int rows, int cols) {
+void gradient_2point(float* image, float* imGc, float* imGr, int rows,
+                     int cols) {
   int c0, r0, c1, r1;
   for (int j = 1; j < rows; j++) {
     r1 = j * cols;
@@ -18,27 +43,30 @@ void gradient_2point(float *image, float *imGc, float *imGr, int rows, int cols)
 
 // as in https://www.nature.com/articles/s41592-022-01669-y#MOESM1
 // 3D Gradient calculation
+void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d,
+                 int h, int w) {
+  float ip0, ip1, ip2, ip3, ip4, ip5, ip6, ip7;
 
-void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d, int h, int w) {
-    float ip0, ip1, ip2, ip3, ip4, ip5, ip6, ip7;
+  int z_i, y_i, x_i;
 
-    int z_i, y_i, x_i;
-
-    for (z_i = 0; z_i < d-1; z_i++) {
-        for (y_i = 0; y_i < h-1; y_i++) {
-            for (x_i = 0; x_i < w-1; x_i++) {
-                ip0 = image[z_i*h*w + y_i*w + x_i];
-                ip1 = image[z_i*h*w + y_i*w + x_i + 1];
-                ip2 = image[z_i*h*w + (y_i+1)*w + x_i];
-                ip3 = image[z_i*h*w + (y_i+1)*w + x_i + 1];
-                ip4 = image[(z_i+1)*h*w + y_i*w + x_i];
-                ip5 = image[(z_i+1)*h*w + y_i*w + x_i + 1];
-                ip6 = image[(z_i+1)*h*w + (y_i+1)*w + x_i];
-                ip7 = image[(z_i+1)*h*w + (y_i+1)*w + x_i + 1];
-                imGx[z_i*h*w + y_i*w + x_i] = (ip1 + ip3 + ip5 + ip7 - ip0 - ip2 - ip4 - ip6) / 4;
-                imGy[z_i*h*w + y_i*w + x_i] = (ip2 + ip3 + ip6 + ip7 - ip0 - ip1 - ip4 - ip5) / 4;
-                imGz[z_i*h*w + y_i*w + x_i] = (ip4 + ip5 + ip6 + ip7 - ip0 - ip1 - ip2 - ip3) / 4;
-            }
-        }
+  for (z_i = 0; z_i < d - 1; z_i++) {
+    for (y_i = 0; y_i < h - 1; y_i++) {
+      for (x_i = 0; x_i < w - 1; x_i++) {
+        ip0 = image[z_i * h * w + y_i * w + x_i];
+        ip1 = image[z_i * h * w + y_i * w + x_i + 1];
+        ip2 = image[z_i * h * w + (y_i + 1) * w + x_i];
+        ip3 = image[z_i * h * w + (y_i + 1) * w + x_i + 1];
+        ip4 = image[(z_i + 1) * h * w + y_i * w + x_i];
+        ip5 = image[(z_i + 1) * h * w + y_i * w + x_i + 1];
+        ip6 = image[(z_i + 1) * h * w + (y_i + 1) * w + x_i];
+        ip7 = image[(z_i + 1) * h * w + (y_i + 1) * w + x_i + 1];
+        imGx[z_i * h * w + y_i * w + x_i] =
+            (ip1 + ip3 + ip5 + ip7 - ip0 - ip2 - ip4 - ip6) / 4;
+        imGy[z_i * h * w + y_i * w + x_i] =
+            (ip2 + ip3 + ip6 + ip7 - ip0 - ip1 - ip4 - ip5) / 4;
+        imGz[z_i * h * w + y_i * w + x_i] =
+            (ip4 + ip5 + ip6 + ip7 - ip0 - ip1 - ip2 - ip3) / 4;
+      }
     }
+  }
 }
--- src/include/_c_gradients.h
+++ src/include/_c_gradients.h
@@ -1,8 +1,13 @@
 #ifndef _C_GRADIENTS_H
 #define _C_GRADIENTS_H
 
-void gradient_2point(float *image, float *imGc, float *imGr, int rows, int cols);
+void _c_gradient_radiality(float* image, float* imGc, float* imGr, int rows,
+                           int cols);
 
-void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d, int h, int w);
+void gradient_2point(float* image, float* imGc, float* imGr, int rows,
+                     int cols);
+
+void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d,
+                 int h, int w);
 
 #endif
