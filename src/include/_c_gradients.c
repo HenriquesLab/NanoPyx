256a0d6bbffaa6a33fb0dc4b95da2f34d41d8af3
Inês Cunha <108281775+inesmcunha@users.noreply.github.com>
2023-04-11 14:28:54 +0100

    added 3d gradient calculation in c

--- src/include/_c_gradients.c
+++ src/include/_c_gradients.c
@@ -3,7 +3,7 @@
 // https://github.com/HenriquesLab/NanoJ-eSRRF/blob/785c71b3bd508c938f63bb780cba47b0f1a5b2a7/resources/liveSRRF.cl
 // under calculateGradient_2point
 void gradient_roberts_cross(float *image, float *imGr, float *imGc, int rows,
-                            int cols) {
+                            int cols) { //TODO: write the Roberts cross gradient (this is 2point)
   int r0, r1, c0, c1;
   for (int i = 1; i < rows; i++) {
     r0 = i * cols;
@@ -29,4 +29,31 @@ void gradient_2point(float *image, float *imGx, float *imGy, int h, int w) {
       imGy[y1 + i] = image[y1 + x1] - image[y0 + x1];
     }
   }
+}
+
+// as in https://www.nature.com/articles/s41592-022-01669-y#MOESM1
+// 3D Gradient calculation
+
+void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d, int h, int w) {
+    float ip0, ip1, ip2, ip3, ip4, ip5, ip6, ip7;
+
+    int z_i, y_i, x_i;
+
+    for (z_i = 0; z_i < d-1; z_i++) {
+        for (y_i = 0; y_i < h-1; y_i++) {
+            for (x_i = 0; x_i < w-1; x_i++) {
+                ip0 = image[z_i*h*w + y_i*w + x_i];
+                ip1 = image[z_i*h*w + y_i*w + x_i + 1];
+                ip2 = image[z_i*h*w + (y_i+1)*w + x_i];
+                ip3 = image[z_i*h*w + (y_i+1)*w + x_i + 1];
+                ip4 = image[(z_i+1)*h*w + y_i*w + x_i];
+                ip5 = image[(z_i+1)*h*w + y_i*w + x_i + 1];
+                ip6 = image[(z_i+1)*h*w + (y_i+1)*w + x_i];
+                ip7 = image[(z_i+1)*h*w + (y_i+1)*w + x_i + 1];
+                imGx[z_i*h*w + y_i*w + x_i] = (ip1 + ip3 + ip5 + ip7 - ip0 - ip2 - ip4 - ip6) / 4;
+                imGy[z_i*h*w + y_i*w + x_i] = (ip2 + ip3 + ip6 + ip7 - ip0 - ip1 - ip4 - ip5) / 4;
+                imGz[z_i*h*w + y_i*w + x_i] = (ip4 + ip5 + ip6 + ip7 - ip0 - ip1 - ip2 - ip3) / 4;
+            }
+        }
+    }
 }
--- src/include/_c_gradients.h
+++ src/include/_c_gradients.h
@@ -6,4 +6,6 @@ void gradient_roberts_cross(float *image, float *imGr, float *imGc, int rows,
 
 void gradient_2point(float *image, float *imGx, float *imGy, int h, int w);
 
+void gradient_3d(float* image, float* imGx, float* imGy, float* imGz, int d, int h, int w);
+
 #endif
