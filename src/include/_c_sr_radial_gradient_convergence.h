9c6e9f52f76f68a89140b376cfbae76bd1651d9a
Inês Cunha <108281775+inesmcunha@users.noreply.github.com>
2023-04-19 14:03:23 +0100

    added a C file for gradient convergence and distance weight

--- src/include/_c_sr_radial_gradient_convergence.h
+++ src/include/_c_sr_radial_gradient_convergence.h
@@ -3,17 +3,8 @@
 
 #include <math.h>
 
-double _c_calculate_dw(double distance, double tSS) {
-  return pow((distance * exp((-distance * distance) / tSS)), 4);
-}
+double _c_calculate_dw(double distance, double tSS);
 
-double _c_calculate_dk(float Gx, float Gy, float dx, float dy, float distance) {
-  float Dk = fabs(Gy * dx - Gx * dy) / sqrt(Gx * Gx + Gy * Gy);
-  if (isnan(Dk)) {
-    Dk = distance;
-  }
-  Dk = 1 - Dk / distance;
-  return Dk;
-}
+double _c_calculate_dk(float Gx, float Gy, float dx, float dy, float distance);
 
 #endif
