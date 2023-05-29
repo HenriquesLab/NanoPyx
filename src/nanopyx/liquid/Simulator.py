9279172655ec5770693a92add637af41527fa4f2
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-05-29 16:49:10 +0100

    Comparing the effect of having a penalty function

--- src/nanopyx/liquid/Simulator.py
+++ src/nanopyx/liquid/Simulator.py
@@ -9,26 +9,34 @@ class Simulator:
     2. Each gear used
     3. Time for each gear
     """
-    def __init__(self, *args, max_iter = 100) -> None:
+    def __init__(self, *args, max_iter = 1000, penalty=True) -> None:
         
         self.rng = np.random.default_rng()
         self.method_objects = args
 
-        self.history = np.empty((100,len(self.method_objects))).astype(str)
-        self.history[:,:] = 'N/A'
-        
         self.max_iter = max_iter
         self.current_iter = 0
+
+        self.history = np.empty((self.max_iter,len(self.method_objects))).astype(str)
+        self.history[:,:] = 'N/A'
+        
+        self.times = np.zeros(self.max_iter)
+
+        self.penalty=penalty
 
     def next_iter(self,):
 
         states = []
+        time = 0
         for met in self.method_objects:
             gear = self.rng.choice(len(ALL_GEARS),1,p=met.probability_vector)[0]
             states.append(ALL_GEARS[gear])
-            met.penalty(self.current_iter,gear)
+            time += met.time_samples[self.current_iter, gear]
+            if self.penalty:
+                met.penalty(self.current_iter,gear)
 
         self.history[self.current_iter,:] = states
+        self.times[self.current_iter] = time
         self.current_iter += 1
 
     def run_iter(self):
@@ -45,6 +53,7 @@ class Simulator:
             
             print(f"{met.name} : {met.probability_vector}")
 
+        print(f"AVERAGE TIME = {np.average(self.times)}")
         print("#######################")
         
 
@@ -53,7 +62,7 @@ if __name__ == "__main__":
     
     # This one prefers gpus
     method_1 = SimMethod('1')
-    method_1.assign_times_to_gears([10,15,20,21,22,21,21,23,100],[1,1,1,1,1,1,1,1,10])
+    method_1.assign_times_to_gears([10,10,30,31,32,31,31,40,100],[1,1,2,2,2,2,2,2,10])
     
     # This one prefers cpus
     method_2 = SimMethod('2')
@@ -63,10 +72,26 @@ if __name__ == "__main__":
     method_3 = SimMethod('3')
     method_3.assign_times_to_gears([10,10,10,10,10,11,10,10,25],[1,1,1,1,1,1,1,1,10])
 
-    sim = Simulator(method_1,method_2,method_3)
-
+    print("NO PENALTY")
+    sim = Simulator(method_1,method_2,method_3,penalty=False)
     sim.print_methods()
     sim.run_iter()
     sim.print_methods()
-
     
+    # This one prefers gpus
+    method_1 = SimMethod('1')
+    method_1.assign_times_to_gears([10,10,30,31,32,31,31,40,100],[1,1,2,2,2,2,2,2,10])
+    
+    # This one prefers cpus
+    method_2 = SimMethod('2')
+    method_2.assign_times_to_gears([20,25,10,11,10,10,11,13,50],[1,1,1,1,1,1,1,1,10])
+
+    # This one prefers nothing
+    method_3 = SimMethod('3')
+    method_3.assign_times_to_gears([10,10,10,10,10,11,10,10,25],[1,1,1,1,1,1,1,1,10])
+
+    print("PENALTY")
+    sim = Simulator(method_1,method_2,method_3,penalty=True)
+    sim.print_methods()
+    sim.run_iter()
+    sim.print_methods()
