a08f3873f84155d1835784b762a702de9582ab2d
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-05-29 10:24:01 +0100

    Added sampling of the simmethods

--- src/nanopyx/liquid/liquid_state_sim_v2.py
+++ src/nanopyx/liquid/liquid_state_sim_v2.py
@@ -42,7 +42,7 @@ class SimMethod:
         # Start with a simple inversely proportional example
         probabilities = (1/self.avg_times)/np.sum(1/self.avg_times)
 
-        assert np.sum(probabilities)==1
+        assert np.allclose(np.sum(self.probability_vector),1)
 
         self.probability_vector = probabilities
 
@@ -53,8 +53,40 @@ class SimMethod:
     
 if __name__ == "__main__":
 
+    """
+    ALL_GEARS = ['OPENCL_1', 'OPENCL_2',
+            'CYTHON_THREADED','CYTHON_THREADED_DYNAMIC', 
+            'CYTHON_THREADED_GUIDED', 'CYTHON_THREADED_STATIC',
+            'CYTHON_UNTHREADED', 'NUMBA', 'PYTHON']
+    """
+
+    # This one prefers gpus
     method_1 = SimMethod('1')
+    method_1.assign_times_to_gears([10,15,20,21,22,21,21,23,100],[1,1,1,1,1,1,1,1,10])
+    
+    # This one prefers cpus
     method_2 = SimMethod('2')
+    method_2.assign_times_to_gears([20,25,10,11,10,10,11,13,50],[1,1,1,1,1,1,1,1,10])
+
+    # This one prefers nothing
     method_3 = SimMethod('3')
-    method_4 = SimMethod('4')
-    method_5 = SimMethod('5')
+    method_3.assign_times_to_gears([10,10,10,10,10,11,10,10,25],[1,1,1,1,1,1,1,1,10])
+
+    methods = [method_1,method_2,method_3]
+    n_methods = len(methods)
+
+    iter_n = 100
+    all_history = np.empty((100,n_methods+1)).astype(str)
+
+    rng = np.random.default_rng()
+
+    for i in range(iter_n):
+        state_per_iter = ['START',]
+        for n in range(len(methods)):    
+            gear = rng.choice(len(ALL_GEARS),1, p=methods[n].probability_vector)[0]
+            state_per_iter.append(ALL_GEARS[gear])
+        
+        all_history[i,:] = state_per_iter
+
+
+    print(all_history)
