44310cfd3a3ee8211bb074fa6fe4d78383d48d7a
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-06-15 16:50:17 +0100

    style

--- src/nanopyx/liquid/__agent__.py
+++ src/nanopyx/liquid/__agent__.py
@@ -10,38 +10,38 @@ from .__opencl__ import opencl_works, devices
 
 class Agent_:
 
-    """
-    Base class for the Agent of the Nanopyx Liquid Engine 
-    Pond, James Pond
-    """
+  """
+  Base class for the Agent of the Nanopyx Liquid Engine 
+  Pond, James Pond
+  """
+
+  def __init__(self,) -> None:
+      """
+      Initialize the Agent
+      The agent is supposed to work as a singleton object, initialized only once in the __init__.py of nanopyx
+      PS: (Is this good enough or is it necessary to implement the singleton design pattern?)
+
+      Agent responsabilities:
+          1. Store the current state of the machine (e.g. OS, CPU, RAM, GPU, Python version etc.);
+          2. Store the current state of ALL initialized LE objects (e.g. anything that is currently running, anything that is scheduled to run,
+            runs previously executed in the current session etc.);
+          3. Whenever a LE object wants to run, it must query the Agent on what is the best implementation for it;
+      """
+
+      ### MACHINE INFO ###
+      self.os_info = {'OS':platform.platform(),'Architecture':platform.machine()}
+      self.cpu_info = {'CPU':platform.processor()}
+      self.ram_info = {'RAM':'TBD'}
+      self.py_info = {'Version':platform.python_version(),'Implementation':platform.python_implementation(),'Compiler':platform.python_compiler()}
+
+      self.numba_info = {'Numba':njit_works()}
+      self.pyopencl_info = {'PyOpenCL':opencl_works(),'Devices':devices}
+      self.cuda_info = {'CUDA':'TBD'}
+      ### MACHINE INFO ###
 
 
-    def __init__(self,) -> None:
-        """
-        Initialize the Agent
-        The agent is supposed to work as a singleton object, initialized only once in the __init__.py of the LE
-        PS: (Is this good enough or is it necessary to implement the singleton design pattern?)
+      self._current_runs = []
+      self._scheduled_runs = []
+      self._finished_runs = []
 
-        Agent responsabilities:
-            1. Store the current state of the machine (e.g. OS, CPU, RAM, GPU, Python version etc.);
-            2. Store the current state of ALL initialized LE objects (e.g. anything that is currently running, anything that is scheduled to run,
-              runs previously executed in the current session etc.);
-            3. Whenever a LE object wants to run, it must query the Agent on what is the best implementation for it;
-        """
-
-        ### MACHINE INFO ###
-        self.os_info = {'OS':platform.platform(),'Architecture':platform.machine()}
-        self.cpu_info = {'CPU':platform.processor()}
-        self.ram_info = {'RAM':'TBD'}
-        self.py_info = {'Version':platform.python_version(),'Implementation':platform.python_implementation(),'Compiler':platform.python_compiler()}
-
-        self.numba_info = {'Numba':njit_works()}
-        self.pyopencl_info = {'PyOpenCL':opencl_works(),'Devices':devices}
-        self.cuda_info = {'CUDA':'TBD'}
-        ### MACHINE INFO ###
-
-
-        self._current_runs = []
-        self._scheduled_runs = []
-        self._finished_runs = []
-
+  
