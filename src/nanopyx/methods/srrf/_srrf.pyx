1ba16ebd902aff978fce0073165cdc609d3e0f2c
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-06-30 14:02:51 +0100

    changed srrf to work with liquid engine

--- src/nanopyx/methods/srrf/_srrf.pyx
+++ src/nanopyx/methods/srrf/_srrf.pyx
@@ -1,6 +1,6 @@
-# cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3, profile=False, autogen_pxd=False
+# cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3, profile=False, autogen_pxd=True
 
-from ...core.transform.sr_radiality cimport Radiality
+from ...liquid._le_radiality import Radiality
 from ...core.transform.sr_temporal_correlations import *
 
 import numpy as np
@@ -8,12 +8,12 @@ cimport numpy as np
 
 from tqdm import tqdm
 
-cdef class SRRF:
-    cdef Radiality radiality
-    cdef float[:] _shift_x, _shift_y
-    cdef int magnification, border
-    cdef float ringRadius
-    cdef bint radialityPositivityConstraint, doIntensityWeighting
+class SRRF:
+    # cdef Radiality radiality
+    # cdef float[:] _shift_x, _shift_y
+    # cdef int magnification, border
+    # cdef float ringRadius
+    # cdef bint radialityPositivityConstraint, doIntensityWeighting
 
     def __init__(self, magnification: int = 5,
                  ringRadius: float = 0.5,
@@ -27,7 +27,7 @@ cdef class SRRF:
         self.radialityPositivityConstraint = radialityPositivityConstraint
         self.doIntensityWeighting = doIntensityWeighting
 
-        self.radiality = Radiality(magnification, ringRadius, border, radialityPositivityConstraint, doIntensityWeighting)
+        self.radiality = Radiality()
 
     def calculate(self, dataset: np.ndarray, int frames_per_timepoint, int SRRForder = 1):
 
@@ -42,11 +42,10 @@ cdef class SRRF:
         with tqdm(total=dataset.shape[0] // frames_per_timepoint, desc="Calculating SRRF", unit="frame") as pbar:
             for i in range(dataset.shape[0] // frames_per_timepoint):
                 data_block = dataset[i*frames_per_timepoint:(i+1)*frames_per_timepoint]
+                data_block_radiality, data_block_intensity = self.radiality.run(data_block, magnification=self.magnification, ringRadius=self.ringRadius, border=self.border, radialityPositivityConstraint=self.radialityPositivityConstraint, doIntensityWeighting=self.doIntensityWeighting)[:2]
 
-                data_block_radiality, data_block_intensity = self.radiality.calculate(data_block)[:2]
-
-                data_block_srrf = calculate_SRRF_temporal_correlations(data_block_radiality, SRRForder)
-                data_block_intensity = calculate_SRRF_temporal_correlations(data_block_intensity, SRRForder)
+                data_block_srrf = calculate_SRRF_temporal_correlations(([data_block_radiality]), SRRForder)
+                data_block_intensity = calculate_SRRF_temporal_correlations([data_block_intensity], SRRForder)
 
                 # data_block_srrf = np.asarray(data_block_radiality).mean(axis=0) 
                 # data_block_intensity = np.asarray(data_block_intensity).mean(axis=0)
