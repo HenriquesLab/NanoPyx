e58daf8853e66da14552c0573c4ec2bb2ab582ca
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-07-04 15:05:00 +0100

    radiality opencl kernel support on LE

--- src/nanopyx/liquid/_le_radiality.pyx
+++ src/nanopyx/liquid/_le_radiality.pyx
@@ -7,6 +7,7 @@ from cython.parallel import parallel, prange
 
 from libc.math cimport sqrt, pi, fabs, cos, sin
 from .__liquid_engine__ import LiquidEngine
+from .__opencl__ import cl, cl_array
 from .__interpolation_tools__ import check_image
 from nanopyx.liquid import CRShiftAndMagnify
 from nanopyx.core.utils.timeit import timeit2
@@ -32,7 +33,7 @@ class Radiality(LiquidEngine):
         self._designation = "Radiality"
         super().__init__(clear_benchmarks=clear_benchmarks, testing=testing,
                         unthreaded_=True, threaded_=True, threaded_static_=True, 
-                        threaded_dynamic_=True, threaded_guided_=True)
+                        threaded_dynamic_=True, threaded_guided_=True, opencl_=True)
 
     
     @timeit2
@@ -125,8 +126,6 @@ class Radiality(LiquidEngine):
         return imRad
         # tag-end
 
-
-        #TODO: fix tag2tag, it couldnt replace by a prange(start,end,schedule)
     def _run_threaded_static(self, float[:,:,:] image, float[:,:,:] image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True):
 
         cdef int _magnification = magnification
@@ -198,7 +197,7 @@ class Radiality(LiquidEngine):
                             imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w) * image_interp[f, j, i]
                         else:
                             imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w)
-
+        
         return imRad
 
     def _run_threaded_guided(self, float[:,:,:] image, float[:,:,:] image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True):
@@ -237,3 +236,87 @@ class Radiality(LiquidEngine):
                             imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w)
 
         return imRad
+
+    
+    def _run_opencl(self, image, image_interp, magnification, ringRadius, border, radialityPositivityConstraint, doIntensityWeighting, dict device):
+
+        cl_ctx = cl.Context([device['device']])
+        cl_queue = cl.CommandQueue(cl_ctx)
+
+        # Swap row and columns because opencl is strange and stores the
+        # array in a buffer in fortran ordering despite the original
+        # numpy array being in C order.
+        #image = np.ascontiguousarray(np.swapaxes(image, 1, 2), dtype=np.float32)
+        #image_interp = np.ascontiguousarray(np.swapaxes(image_interp, 1, 2), dtype=np.float32)
+
+        code = self._get_cl_code("_le_radiality.cl", device['DP'])
+
+        cdef int _magnification = magnification
+        cdef int _border = border
+        cdef float _ringRadius = ringRadius * magnification
+        cdef int _doIntensityWeighting = doIntensityWeighting
+        cdef int _radialityPositivityConstraint = radialityPositivityConstraint
+        cdef int nRingCoordinates = 12
+        cdef float angleStep = (pi * 2.) / nRingCoordinates
+        cdef float[12] xRingCoordinates, yRingCoordinates
+
+        with nogil:
+            for angleIter in range(nRingCoordinates):
+                xRingCoordinates[angleIter] = cos(angleStep * angleIter) * _ringRadius
+                yRingCoordinates[angleIter] = sin(angleStep * angleIter) * _ringRadius
+
+    
+        cdef int nFrames = image.shape[0]
+        cdef int h = image.shape[1]
+        cdef int w = image.shape[2]
+
+        cdef float [:,:,:] imGx = np.zeros_like(image) 
+        cdef float [:,:,:] imGy = np.zeros_like(image)
+
+        cdef float[:,:,:] image_MV = image
+        with nogil:
+            for f in range(nFrames):
+                _c_gradient_radiality(&image_MV[f,0,0], &imGx[f,0,0], &imGy[f,0,0], h, w)
+
+        image_in = cl_array.to_device(cl_queue, image)
+        imageinter_in = cl_array.to_device(cl_queue, image_interp)
+        imGx_in = cl_array.to_device(cl_queue, np.array(imGx, dtype=np.float32))
+        imGy_in = cl_array.to_device(cl_queue, np.array(imGy, dtype=np.float32))
+        imRad_out = cl_array.zeros(cl_queue, (nFrames, h*magnification, w*magnification), dtype=np.float32)
+
+        xRingCoordinates_in = cl_array.to_device(cl_queue, np.array(xRingCoordinates, dtype=np.float32))
+        yRingCoordinates_in = cl_array.to_device(cl_queue, np.array(yRingCoordinates, dtype=np.float32))
+
+        # Grid size
+        lowest_row = (1 + border) * magnification 
+        highest_row = (h - 1 - border) * magnification
+
+        lowest_col = (1 + _border) * _magnification
+        highest_col = (w - 1 - _border) * _magnification
+
+        prg = cl.Program(cl_ctx, code).build()  
+ 
+        prg.radiality(
+            cl_queue,
+            (nFrames, highest_row - lowest_row, highest_col - lowest_col),
+            None,
+            image_in.data,
+            imageinter_in.data,
+            imGx_in.data,
+            imGy_in.data,
+            imRad_out.data,
+            xRingCoordinates_in.data,
+            yRingCoordinates_in.data,
+            np.int32(magnification),
+            np.float32(ringRadius),
+            np.int32(nRingCoordinates),
+            np.int32(radialityPositivityConstraint),
+            np.int32(border),
+            np.int32(h),
+            np.int32(w)
+        )
+    
+        cl_queue.finish()        
+        
+        return np.asarray(imRad_out.get(),dtype=np.float32)
+    
