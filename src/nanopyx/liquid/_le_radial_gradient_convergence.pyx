c7b936aacc85b51795043477c4b743448988d9ba
Inês Cunha <108281775+inesmcunha@users.noreply.github.com>
2023-07-10 19:12:17 +0100

    added pyOpenCL implementation of RGC (untested)

--- src/nanopyx/liquid/_le_radial_gradient_convergence.pyx
+++ src/nanopyx/liquid/_le_radial_gradient_convergence.pyx
@@ -7,6 +7,7 @@ cimport numpy as np
 from cython.parallel import parallel, prange
 
 from libc.math cimport sqrt, pow
+from .__opencl__ import cl, cl_array
 from .__liquid_engine__ import LiquidEngine
 from .__interpolation_tools__ import check_image
 from nanopyx.liquid import CRShiftAndMagnify
@@ -28,7 +29,7 @@ class RadialGradientConvergence(LiquidEngine):
         self._designation = "RGC"
         super().__init__(clear_benchmarks=clear_benchmarks, testing=testing,
                         unthreaded_=True, threaded_=True, threaded_static_=True, 
-                        threaded_dynamic_=True, threaded_guided_=True)
+                        threaded_dynamic_=True, threaded_guided_=True, opencl_=True)
 
 
 
@@ -273,6 +274,73 @@ class RadialGradientConvergence(LiquidEngine):
         # tag-end
 
     
+    def _run_opencl(self, image, magnification, radius, sensitivity, doIntensityWeighting, dict device):
+
+        cl_ctx = cl.Context([device['device']])
+        cl_queue = cl.CommandQueue(cl_ctx)
+
+        code = self._get_cl_code("_le_radial_gradient_convergence.cl", device['DP'])
+
+        cdef float sigma = radius / 2.355
+        cdef float fwhm = radius
+        cdef float tSS = 2 * sigma * sigma
+        cdef float tSO = 2 * sigma + 1
+        cdef float Gx_Gy_MAGNIFICATION = 2.0
+        cdef int _magnification = magnification
+        cdef float _sensitivity = sensitivity
+        cdef int _doIntensityWeighting = doIntensityWeighting
+
+        cdef int nFrames = image.shape[0]
+        cdef int rows = image.shape[1]
+        cdef int cols = image.shape[2]
+        cdef int rowsM = <int>(rows * magnification)
+        cdef int colsM = <int>(cols * magnification)
+
+        crsm = CRShiftAndMagnify()
+        cdef float [:,:,:] image_interp = crsm.run(image, 0, 0, magnification, magnification)
+
+        cdef float [:,:,:] gradient_col = np.zeros_like(image) 
+        cdef float [:,:,:] gradient_row = np.zeros_like(image)
+        cdef float[:,:,:] image_MV = image
+
+        cdef int n
+        with nogil: 
+            for n in range(nFrames):
+                _c_gradient_roberts_cross(&image_MV[n,0,0], &gradient_col[n,0,0], &gradient_row[n,0,0], rows, cols)
+
+        cdef float [:,:,:] grad_col_int = crsm.run(gradient_col, 0, 0, magnification*Gx_Gy_MAGNIFICATION, magnification*Gx_Gy_MAGNIFICATION)
+        cdef float [:,:,:] grad_row_int = crsm.run(gradient_row, 0, 0, magnification*Gx_Gy_MAGNIFICATION, magnification*Gx_Gy_MAGNIFICATION)
+
+        grad_col_int_in = cl_array.to_device(cl_queue, np.array(grad_col_int, dtype=np.float32))
+        grad_row_int_in = cl_array.to_device(cl_queue, np.array(grad_row_int, dtype=np.float32))
+        image_interp_in = cl_array.to_device(cl_queue, image_interp)
+        rgc_map_out = cl_array.zeros(cl_queue, (nFrames, rowsM, colsM), dtype=np.float32)
+
+        # Grid size
+        prg = cl.Program(cl_ctx, code).build() 
+
+        prg.calculate_rgc(cl_queue, 
+                    (nFrames, rowsM, colsM), 
+                    None, 
+                    grad_col_int_in.data,
+                    grad_row_int_in.data,
+                    image_interp_in.data,
+                    rgc_map_out.data,
+                    np.int32(cols), 
+                    np.int32(rows), 
+                    np.int32(_magnification), 
+                    np.float32(Gx_Gy_MAGNIFICATION), 
+                    np.float32(fwhm), 
+                    np.float32(tSO), 
+                    np.float32(tSS), 
+                    np.float32(_sensitivity), 
+                    np.int32(_doIntensityWeighting) )
+        
+        cl_queue.finish()        
+        
+        return np.asarray(rgc_map_out.get(),dtype=np.float32)
+                
+
 
 
 
