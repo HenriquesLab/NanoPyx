b6b5958eefaa170d394b6684d4fadedcdf7a0e91
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-07-21 11:32:12 +0100

    Gpu calculation updating

--- src/nanopyx/liquid/_le_radial_gradient_convergence.pyx
+++ src/nanopyx/liquid/_le_radial_gradient_convergence.pyx
@@ -196,11 +196,11 @@ class RadialGradientConvergence(LiquidEngine):
         # image_interp = mag*size
         # output = image_interp
 
+        # Context and queue
         cl_ctx = cl.Context([device['device']])
         cl_queue = cl.CommandQueue(cl_ctx)
 
-        code = self._get_cl_code("_le_radial_gradient_convergence.cl", device['DP'])
-
+        # Parameters
         cdef float sigma = radius / 2.355
         cdef float fwhm = radius
         cdef float tSS = 2 * sigma * sigma
@@ -210,44 +210,80 @@ class RadialGradientConvergence(LiquidEngine):
         cdef float _sensitivity = sensitivity
         cdef int _doIntensityWeighting = doIntensityWeighting
 
+        # Sizes
         cdef int nFrames = gradient_col_interp.shape[0]
         cdef int rows_interpolated = <int>(gradient_row_interp.shape[1] / Gx_Gy_MAGNIFICATION)
         cdef int cols_interpolated = <int>(gradient_row_interp.shape[2] / Gx_Gy_MAGNIFICATION)
 
-        grad_col_int_in = cl_array.to_device(cl_queue, np.asarray(gradient_col_interp, dtype=np.float32))
-        grad_row_int_in = cl_array.to_device(cl_queue, np.asarray(gradient_row_interp, dtype=np.float32))
-        image_interp_in = cl_array.to_device(cl_queue, np.asarray(image_interp, dtype=np.float32))
-        rgc_map_out = cl_array.zeros(cl_queue, (nFrames, rows_interpolated, cols_interpolated), dtype=np.float32)
-
-        # Grid size
+        # Grid size of the global work space
         lowest_row = _magnification*2 
         highest_row = rows_interpolated - _magnification*2
-
         lowest_col = _magnification*2
         highest_col =  cols_interpolated - _magnification*2
+        global_work_space = (nFrames, highest_row - lowest_row, highest_col - lowest_col)
 
+        # Output 
+        rgc_map = np.zeros((nFrames, rows_interpolated, cols_interpolated), dtype=np.float32)
+
+        # Calculating max slices
+        size_per_slice = gradient_col_interp[0,:,:].nbytes + gradient_row_interp[0,:,:].nbytes + image_interp[0,:,:].nbytes + rgc_map[0,:,:].nbytes
+        max_slices = int((device['device'].global_mem_size // (size_per_slice))/3)   # TODO 3 is a magic number 
+
+        # Initial buffers
+        mf = cl.mem_flags
+        grad_col_int_in = cl.Buffer(cl_ctx, mf.READ_ONLY, gradient_col_interp[0:max_slices,:,:].nbytes)
+        cl.enqueue_copy(cl_queue, grad_col_int_in, gradient_col_interp[0:max_slices,:,:])
+
+        grad_row_int_in = cl.Buffer(cl_ctx, mf.READ_ONLY, gradient_row_interp[0:max_slices,:,:].nbytes)
+        cl.enqueue_copy(cl_queue, grad_row_int_in, grad_row_int_in[0:max_slices,:,:])
+
+        image_interp_in = cl.Buffer(cl_ctx, mf.READ_ONLY, image_interp[0:max_slices,:,:].nbytes)
+        cl.enqueue_copy(cl_queue, image_interp_in, image_interp[0:max_slices,:,:])
+
+        rgc_map_out = cl.Buffer(cl_ctx, mf.WRITE_ONLY, rgc_map[0:max_slices,:,:].nbytes)
+
+        # Code and building the kernel
+        code = self._get_cl_code("_le_radial_gradient_convergence.cl", device['DP'])
         prg = cl.Program(cl_ctx, code).build() 
+        knl = prg.calculate_rgc
 
-        prg.calculate_rgc(cl_queue, 
-                    (nFrames, highest_row - lowest_row, highest_col - lowest_col), 
-                    None, 
-                    grad_col_int_in.data,
-                    grad_row_int_in.data,
-                    image_interp_in.data,
-                    rgc_map_out.data,
-                    np.int32(cols_interpolated), 
-                    np.int32(rows_interpolated), 
-                    np.int32(_magnification), 
-                    np.float32(Gx_Gy_MAGNIFICATION), 
-                    np.float32(fwhm), 
-                    np.float32(tSO), 
-                    np.float32(tSS), 
-                    np.float32(_sensitivity), 
-                    np.int32(_doIntensityWeighting) )
+        # Actual computation
+        for i in range(0, nFrames-1, max_slices):
+            if nFrames - i >= max_slices:
+                n_slices = max_slices
+            else:
+                n_slices = nFrames - i
+
+            knl(cl_queue, 
+                global_work_space, 
+                self.get_work_group(device['device'],global_work_space), 
+                grad_col_int_in,
+                grad_row_int_in,
+                image_interp_in,
+                rgc_map_out,
+                np.int32(cols_interpolated), 
+                np.int32(rows_interpolated), 
+                np.int32(_magnification), 
+                np.float32(Gx_Gy_MAGNIFICATION), 
+                np.float32(fwhm), 
+                np.float32(tSO), 
+                np.float32(tSS), 
+                np.float32(_sensitivity), 
+                np.int32(_doIntensityWeighting) )
+            
+            # Copy output
+            cl.enqueue_copy(cl_queue, rgc_map[i:i+n_slices,:,:], rgc_map_out)
+
+            # Copy input
+            if i<=nFrames-max_slices:
+                cl.enqueue_copy(cl_queue, grad_col_int_in, gradient_col_interp[i+n_slices:i+2*n_slices,:,:])
+                cl.enqueue_copy(cl_queue, grad_row_int_in, gradient_row_interp[i+n_slices:i+2*n_slices,:,:])
+                cl.enqueue_copy(cl_queue, image_interp_in, image_interp[i+n_slices:i+2*n_slices,:,:]) 
+
+            cl_queue.finish()
+
         
-        cl_queue.finish()        
-        
-        return np.asarray(rgc_map_out.get(),dtype=np.float32)
+        return rgc_map
         
 
 
