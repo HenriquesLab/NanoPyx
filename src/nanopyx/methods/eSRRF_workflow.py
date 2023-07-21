1a5cd56aeea27a2e7e8b1a917ec33ccc2a29a98a
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-07-21 15:18:38 +0100

    Fixing keyword arg

--- src/nanopyx/methods/eSRRF_workflow.py
+++ src/nanopyx/methods/eSRRF_workflow.py
@@ -10,7 +10,7 @@ def eSRRF(image, magnification: int = 5, radius: float = 1.5, sensitivity: float
       Gx_Gy_MAGNIFICATION = 2.0
 
       _eSRRF = Workflow((CRShiftAndMagnify(), (image, 0, 0, magnification, magnification), {}),
-                        (GradientRobertsCross(), (image,), {"run_type": gradrc_runtype}),
+                        (GradientRobertsCross(), (image,), {}),
                         (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_0', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {}),
                         (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_1', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {}),
                         (RadialGradientConvergence(), ('PREV_RETURN_VALUE_2_0', 'PREV_RETURN_VALUE_3_0', 'PREV_RETURN_VALUE_0_0'), {'magnification': magnification, 'radius': radius, 'sensitivity': sensitivity,'doIntensityWeighting': doIntensityWeighting}))
