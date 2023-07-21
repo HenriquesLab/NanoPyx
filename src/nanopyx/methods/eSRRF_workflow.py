836e545ddf30f5b22544a01b690fd713e7b69951
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-07-21 17:05:00 +0100

    Temporarily removed control of the agent

--- src/nanopyx/methods/eSRRF_workflow.py
+++ src/nanopyx/methods/eSRRF_workflow.py
@@ -3,16 +3,17 @@ from ..liquid import  CRShiftAndMagnify, RadialGradientConvergence, GradientRobe
 import numpy as np
 
 
-def eSRRF(image, magnification: int = 5, radius: float = 1.5, sensitivity: float = 1, doIntensityWeighting: bool = True):
+def eSRRF(image, magnification: int = 5, radius: float = 1.5, sensitivity: float = 1, doIntensityWeighting: bool = True,
+          interp_runtype1="Threaded", interp_runtype2="Threaded", interp_runtype3="Threaded", gradrc_runtype="Threaded", rgc_runtype="Threaded"):
       """
       eSRRF analysis of an image
       """
       Gx_Gy_MAGNIFICATION = 2.0
 
-      _eSRRF = Workflow((CRShiftAndMagnify(), (image, 0, 0, magnification, magnification), {}),
-                        (GradientRobertsCross(), (image,), {}),
-                        (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_0', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {}),
-                        (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_1', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {}),
-                        (RadialGradientConvergence(), ('PREV_RETURN_VALUE_2_0', 'PREV_RETURN_VALUE_3_0', 'PREV_RETURN_VALUE_0_0'), {'magnification': magnification, 'radius': radius, 'sensitivity': sensitivity,'doIntensityWeighting': doIntensityWeighting}))
+      _eSRRF = Workflow((CRShiftAndMagnify(), (image, 0, 0, magnification, magnification), {'run_type':interp_runtype1}),
+                        (GradientRobertsCross(), (image,), {'run_type':gradrc_runtype}),
+                        (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_0', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {'run_type':interp_runtype2}),
+                        (CRShiftAndMagnify(), ('PREV_RETURN_VALUE_1_1', 0, 0, magnification * Gx_Gy_MAGNIFICATION, magnification * Gx_Gy_MAGNIFICATION), {'run_type':interp_runtype3}),
+                        (RadialGradientConvergence(), ('PREV_RETURN_VALUE_2_0', 'PREV_RETURN_VALUE_3_0', 'PREV_RETURN_VALUE_0_0'), {'magnification': magnification, 'radius': radius, 'sensitivity': sensitivity,'doIntensityWeighting': doIntensityWeighting,'run_type':rgc_runtype}))
 
       return _eSRRF
