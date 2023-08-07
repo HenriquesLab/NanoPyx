72092c45af62a93371c0b7afe977a2f15f493578
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-08-07 15:23:40 +0100

    added esrrf workflow class to init

--- src/nanopyx/methods/__init__.py
+++ src/nanopyx/methods/__init__.py
@@ -4,4 +4,5 @@ from . import drift_alignment
 from . import channel_registration
 
 from .SRRF_workflow import SRRF as SRRF_w
+from .eSRRF_workflow import eSRRF_3 as eSRRF_w
 from .dummy_conv_workflow import ConvolutionWorkflow as Conv2D_w
