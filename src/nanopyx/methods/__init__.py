e3d8f47ed50d7d543dae7eeb22fd9e5abd42bbcb
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-09-14 15:38:59 +0100

    updating init methods

--- src/nanopyx/methods/__init__.py
+++ src/nanopyx/methods/__init__.py
@@ -1,8 +1,5 @@
-from . import esrrf
-from . import squirrel
 from . import drift_alignment
 from . import channel_registration
 
-from .SRRF_workflow import SRRF as SRRF_w
-from .eSRRF_workflow import eSRRF_3 as eSRRF_w
-from .dummy_conv_workflow import ConvolutionWorkflow as Conv2D_w
+from .esrrf import eSRRF_workflow
+from .srrf import SRRF_workflow
