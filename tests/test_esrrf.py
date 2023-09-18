02b8fa5efbbe862754a89e6c4b8c829a23e75aed
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-09-18 16:47:16 +0100

    Added new tests

---
+++ tests/test_esrrf.py
@@ -0,0 +1,27 @@
+from nanopyx.core.transform import RadialGradientConvergence 
+from nanopyx.core.transform import CRShiftAndMagnify, GradientRobertsCross
+from nanopyx.methods import eSRRF
+
+def test_rgc(downloader):
+
+    dataset = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=True)
+    small_dataset = dataset[:10,:20,:20]
+
+    crsm = CRShiftAndMagnify()
+    grc = GradientRobertsCross(testing=True)
+    rgc = RadialGradientConvergence(testing=True)
+    
+    small_dataset_interp = crsm.run(small_dataset, 0, 0, 5, 5)
+    gradient_col, gradient_row = grc.run(small_dataset)
+    gradient_col_interp = crsm.run(gradient_col, 0, 0, 5*2, 5*2)
+    gradient_row_interp = crsm.run(gradient_row, 0, 0, 5*2, 5*2)
+    radial_gradients = rgc.benchmark(gradient_col_interp, gradient_row_interp, small_dataset_interp)
+
+def test_esrrf(downloader):
+
+    dataset = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=True)
+    small_dataset = dataset[:10,:20,:20]
+    
+    eSRRF(small_dataset)
---
+++ tests/test_srrf.py
@@ -0,0 +1,22 @@
+from nanopyx.core.transform import Radiality, CRShiftAndMagnify
+from nanopyx.methods import SRRF
+
+def test_radiality(downloader):
+
+    dataset = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=True)
+    small_dataset = dataset[:10,:20,:20]
+
+    interp = CRShiftAndMagnify()
+    small_dataset_interp = interp.run(small_dataset,0,0,5,5)
+
+    liquid_rad = Radiality(testing=True)
+    imRad = liquid_rad.benchmark(small_dataset,small_dataset_interp)
+
+def test_srrf(downloader):
+
+    dataset = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=True)
+    small_dataset = dataset[:10,:20,:20]
+
+    SRRF(small_dataset)
