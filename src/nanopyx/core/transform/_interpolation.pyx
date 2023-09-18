323d318e2a08ed6badf894ce2381be6be0e7fff9
António Brito <50997716+antmsbrito@users.noreply.github.com>
2023-09-18 16:45:46 +0100

    Fixed bug in indices

--- src/nanopyx/core/transform/_interpolation.pyx
+++ src/nanopyx/core/transform/_interpolation.pyx
@@ -10,8 +10,8 @@ cdef extern from "_c_interpolation_catmull_rom.h":
     float _c_cr_interpolate "_c_interpolate" (float *image, float row, float col, int rows, int cols) 
 
 cdef float _cr_interpolate(float[:, :] img_stack, float row, float col):
-    cdef int rows = img_stack.shape[1]
-    cdef int cols = img_stack.shape[2]
+    cdef int rows = img_stack.shape[0]
+    cdef int cols = img_stack.shape[1]
     return _c_cr_interpolate(&img_stack[0,0], row, col, rows, cols)
 
 def cr_interpolate(img_stack, row, col):
