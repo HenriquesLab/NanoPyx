73efc5f9d0fd7a7140714afe8bb77a7f75759e4e
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-09-27 17:01:15 +0100

    new batch, previous was overcooked

--- src/notebookchef/pantry/methods/decorr.py
+++ src/notebookchef/pantry/methods/decorr.py
@@ -1,33 +1,43 @@
-#@title Create Decorr GUI for original image
-gui_decorr_1 = EasyGui("DecorrAnalysis")
+#@title Create Decorrelation Analysis GUI for eSRRF data
+gui_decorr = EasyGui("DecorrAnalysis")
 
 from nanopyx.core.analysis.decorr import DecorrAnalysis
 
 def run_decorr(b):
     clear_output()
-    gui_decorr_1.show()
-    gui_decorr_1.save_settings()
-    pixel_size = gui_decorr_1["pixel_size"].value
-    units = gui_decorr_1["units"].value
-    rmin = gui_decorr_1["rmin"].value
-    rmax = gui_decorr_1["rmax"].value
-    frame = gui_decorr_1["frame"].value
-    gui_decorr_1["run"].disabled = True
-    gui_decorr_1["run"].description = "Calculating..."
-    global decorr_calculator_raw
-    decorr_calculator_raw = DecorrAnalysis(pixel_size=pixel_size, units=units, rmin=rmin, rmax=rmax)
-    decorr_calculator_raw.run_analysis(dataset_original[frame])
-    gui_decorr_1["run"].disabled = False
-    gui_decorr_1["run"].description = "Calculate"
-    plt.imshow(decorr_calculator_raw.plot_results())
+    gui_decorr.show()
+    gui_decorr.save_settings()
+    pixel_size = gui_decorr["pixel_size"].value
+    units = gui_decorr["units"].value
+    first_frame = gui_decorr["first_frame"].value
+    rmin = gui_decorr["rmin"].value
+    rmax = gui_decorr["rmax"].value
+    gui_decorr["run"].disabled = True
+    gui_decorr["run"].description = "Calculating..."
+    global decorr_calculator
+    decorr_calculator = DecorrAnalysis(pixel_size=pixel_size, units=units, rmin=rmin, rmax=rmax)
+    decorr_calculator.run_analysis(dataset_original[first_frame])
+    gui_decorr["run"].disabled = False
+    gui_decorr["run"].description = "Calculate"
+    plot = decorr_calculator.plot_results()
+    if gui_decorr["save"].value:
+        if own_data:
+            path = gui_data["upload"].selected_path
+            name = gui_data["upload"].selected_filename.split(".")[0]
+            tiff.imwrite(path + os.sep + name + "_eSRRF_decorr_analysis.tif", plot)
+        else:
+            name = gui_data["data_source"].value.replace("Example dataset: ", "")
+            tiff.imwrite(name + "_eSRRF_decorr_analysis.tif", plot)
+    plt.imshow(plot)
     plt.axis("off")
     plt.show()
-    
-gui_decorr_1.add_int_slider("pixel_size", description="Pixel Size:", min=0.01, max=1000, value=100, remember_value=True)
-gui_decorr_1.add_dropdown("units", description="Units: ", options=["nm", "um", "mm"], value="nm")
-gui_decorr_1.add_int_slider("frame", description="Frame:", min=0, max=dataset_original[0].shape[0]-1, value=0)
-gui_decorr_1.add_float_slider("rmin", description="Radius Min:", min=0.0, max=0.5, value=0.0)
-gui_decorr_1.add_float_slider("rmax", description="Radius Max:", min=0.5, max=1.0, value=1.0)
-gui_decorr_1.add_button("run", description="Calculate")
-gui_decorr_1["run"].on_click(run_decorr)
-gui_decorr_1.show()
+
+gui_decorr.add_int_slider("pixel_size", description="Pixel Size:", min=0.01, max=1000, value=100, remember_value=True)
+gui_decorr.add_dropdown("units", description="Units: ", options=["nm", "um", "mm"], value="nm")
+gui_decorr.add_int_slider("first_frame", description="Frame to be used:", min=0, max=dataset_original.shape[0]-1, value=0)
+gui_decorr.add_float_slider("rmin", description="Radius Min:", min=0.0, max=0.5, value=0.0)
+gui_decorr.add_float_slider("rmax", description="Radius Max:", min=0.5, max=1.0, value=1.0)
+gui_decorr.add_checkbox("save", description="Save Output", value=True)
+gui_decorr.add_button("run", description="Calculate")
+gui_decorr["run"].on_click(run_decorr)
+gui_decorr.show()
