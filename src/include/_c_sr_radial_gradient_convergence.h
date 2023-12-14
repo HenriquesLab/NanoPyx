872e5f7959c52138986e21cd3fe68adbb40bf2e3
agvesga <72971822+agvesga@users.noreply.github.com>
2023-12-14 13:37:34 +0000

    added rgd3D and dk3D

--- src/include/_c_sr_radial_gradient_convergence.h
+++ src/include/_c_sr_radial_gradient_convergence.h
@@ -9,4 +9,9 @@ double _c_calculate_dk(float Gx, float Gy, float dx, float dy, float distance);
 
 float _c_calculate_rgc(int xM, int yM, float* imIntGx, float* imIntGy, int colsM, int rowsM, int magnification, float Gx_Gy_MAGNIFICATION, float fwhm, float tSO, float tSS, float sensitivity);
 
+double _c_calculate_dk3D(float Gx, float Gy, float Gz, float dx, float dy, float dz, float distance);
+
+float _c_calculate_rgc3D(int xM, int yM, int sliceM, float* imIntGx, float* imIntGy, float* imIntGz, int colsM, int rowsM, int slicesM, int magnification, float Gx_Gy_MAGNIFICATION, float Gz_MAGNIFICATION, float fwhm, float fwhm_z, float tSO, float tSS, float sensitivity);
+
+
 #endif
