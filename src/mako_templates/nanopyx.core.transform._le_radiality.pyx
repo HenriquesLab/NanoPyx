eff83a9874a2f7ec52ad7fb6f19e850d8a8d5d0d
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-01-11 12:04:13 +0000

    added reamining templates

---
+++ src/mako_templates/nanopyx.core.transform._le_radiality.pyx
@@ -0,0 +1,245 @@
+<%!
+schedulers = ['threaded','threaded_guided','threaded_dynamic','threaded_static']
+%># cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3, profile=False, autogen_pxd=False
+
+import numpy as np
+cimport numpy as np
+
+from cython.parallel import parallel, prange
+
+from libc.math cimport sqrt, pi, fabs, cos, sin
+from ...__liquid_engine__ import LiquidEngine
+from ...__opencl__ import cl, cl_array
+from .__interpolation_tools__ import check_image
+
+from ._le_interpolation_catmull_rom import ShiftAndMagnify as CRShiftAndMagnify
+
+cdef extern from "_c_interpolation_catmull_rom.h":
+    pass
+
+cdef extern from "_c_sr_radiality.h":
+    float _c_calculate_radiality_per_subpixel(int i, int j, float* imGx, float* imGy, float* xRingCoordinates, float* yRingCoordinates, int magnification, float ringRadius, int nRingCoordinates, int radialityPositivityConstraint, int h, int w) nogil
+
+cdef extern from "_c_gradients.h":
+    void _c_gradient_radiality(float* image, float* imGc, float* imGr, int rows,
+                          int cols) nogil
+
+# cdef float Gx_Gy_MAGNIFICATION = 2.0
+
+class Radiality(LiquidEngine):
+    """
+    Radial gradient convergence using the NanoPyx Liquid Engine
+    """
+
+    def __init__(self, clear_benchmarks=False, testing=False):
+        self._designation = "Radiality"
+        super().__init__(clear_benchmarks=clear_benchmarks, testing=testing,
+                        unthreaded_=False, threaded_=True, threaded_static_=True, 
+                        threaded_dynamic_=True, threaded_guided_=True, opencl_=True)
+
+        self._default_benchmarks = {'OpenCL': {"(['shape(100, 100, 100)', 'shape(100, 500, 500)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [62500000000000.0, 0.9875555419998818, 0.9819192090008073, 0.9873175410002659], "(['shape(100, 150, 150)', 'shape(100, 750, 750)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [316406250000000.0, 2.204043416999866, 2.202929583000696, 2.2014064169998164]}, 'Threaded': {"(['shape(100, 100, 100)', 'shape(100, 500, 500)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [62500000000000.0, 3.56841600000007, 3.5533971249997194, 3.2480534169999373], "(['shape(100, 150, 150)', 'shape(100, 750, 750)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [316406250000000.0, 7.231931541999984, 7.29744833300083, 7.1096272920003685]}, 'Threaded_dynamic': {"(['shape(100, 100, 100)', 'shape(100, 500, 500)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [62500000000000.0, 2.7894346250000126, 2.778901292000228, 2.714467834000061], "(['shape(100, 150, 150)', 'shape(100, 750, 750)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [316406250000000.0, 6.166010375000042, 6.16195045799941, 6.185223917000258]}, 'Threaded_guided': {"(['shape(100, 100, 100)', 'shape(100, 500, 500)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [62500000000000.0, 2.7807863329999236, 2.714370166999288, 2.720014749999791], "(['shape(100, 150, 150)', 'shape(100, 750, 750)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [316406250000000.0, 6.414935042000252, 6.137087832999896, 6.186802750000425]}, 'Threaded_static': {"(['shape(100, 100, 100)', 'shape(100, 500, 500)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [62500000000000.0, 3.3877954579998004, 3.466373499999463, 3.2974376660004054], "(['shape(100, 150, 150)', 'shape(100, 750, 750)', 'number(5)', 'number(0.5)', 'number(0)', True, True], {})": [316406250000000.0, 7.016077875000519, 7.207373291999829, 7.122106583999994]}}
+
+    def run(self, image, image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True, run_type = None): 
+        image = check_image(image)
+        image_interp = check_image(image_interp)
+        return self._run(image, image_interp, magnification, ringRadius, border, radialityPositivityConstraint, doIntensityWeighting, run_type=run_type)
+    
+    def benchmark(self, image, image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True): 
+        image = check_image(image)
+        image_interp = check_image(image_interp)
+        return super().benchmark(image, image_interp, magnification, ringRadius, border, radialityPositivityConstraint, doIntensityWeighting)
+
+    def _run_unthreaded(self, float[:,:,:] image, float[:,:,:] image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True):
+
+        cdef int _magnification = magnification
+        cdef int _border = border
+        cdef float _ringRadius = ringRadius * magnification
+        cdef int _doIntensityWeighting = doIntensityWeighting
+        cdef int _radialityPositivityConstraint = radialityPositivityConstraint
+        cdef int nRingCoordinates = 12
+        cdef float angleStep = (pi * 2.) / nRingCoordinates
+        cdef float[12] xRingCoordinates, yRingCoordinates
+
+        with nogil:
+            for angleIter in range(nRingCoordinates):
+                xRingCoordinates[angleIter] = cos(angleStep * angleIter) * _ringRadius
+                yRingCoordinates[angleIter] = sin(angleStep * angleIter) * _ringRadius
+        
+        cdef int nFrames = image.shape[0]
+        cdef int h = image.shape[1]
+        cdef int w = image.shape[2]
+
+        
+        cdef float [:,:,:] imGx = np.zeros_like(image) 
+        cdef float [:,:,:] imGy = np.zeros_like(image)
+        cdef float [:,:,:] imRad = np.zeros((nFrames, h*magnification, w*magnification), dtype=np.float32)
+
+        cdef int f, j, i
+        with nogil:
+            for f in range(nFrames):
+                 _c_gradient_radiality(&image[f,0,0], &imGx[f,0,0], &imGy[f,0,0], h, w)
+                 for j in range((1 + _border) * _magnification, (h - 1 - _border) * _magnification):
+                    for i in range((1 + _border) * _magnification, (w - 1 - _border) * _magnification):
+                        if _doIntensityWeighting:
+                            imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w) * image_interp[f, j, i]
+                        else:
+                            imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w)
+
+        return np.asarray(imRad)
+
+    % for sch in schedulers:
+    def _run_${sch}(self, float[:,:,:] image, float[:,:,:] image_interp, magnification: int = 5, ringRadius: float = 0.5, border: int = 0, radialityPositivityConstraint: bool = True, doIntensityWeighting: bool = True):
+
+        cdef int _magnification = magnification
+        cdef int _border = border
+        cdef float _ringRadius = ringRadius * magnification
+        cdef int _doIntensityWeighting = doIntensityWeighting
+        cdef int _radialityPositivityConstraint = radialityPositivityConstraint
+        cdef int nRingCoordinates = 12
+        cdef float angleStep = (pi * 2.) / nRingCoordinates
+        cdef float[12] xRingCoordinates, yRingCoordinates
+
+        with nogil:
+            for angleIter in range(nRingCoordinates):
+                xRingCoordinates[angleIter] = cos(angleStep * angleIter) * _ringRadius
+                yRingCoordinates[angleIter] = sin(angleStep * angleIter) * _ringRadius
+        
+        cdef int nFrames = image.shape[0]
+        cdef int h = image.shape[1]
+        cdef int w = image.shape[2]
+        
+        cdef float [:,:,:] imGx = np.zeros_like(image) 
+        cdef float [:,:,:] imGy = np.zeros_like(image)
+        cdef float [:,:,:] imRad = np.zeros((nFrames, h*magnification, w*magnification), dtype=np.float32)
+
+        cdef int f, j, i
+        with nogil:
+            for f in range(nFrames):
+                 _c_gradient_radiality(&image[f,0,0], &imGx[f,0,0], &imGy[f,0,0], h, w)
+                 % if sch=='threaded':
+                 for j in prange((1 + _border) * _magnification, (h - 1 - _border) * _magnification):
+                 % else:
+                 for j in prange((1 + _border) * _magnification, (h - 1 - _border) * _magnification, schedule="${sch.split('_')[1]}"):
+                 % endif
+                    for i in range((1 + _border) * _magnification, (w - 1 - _border) * _magnification):
+                        if _doIntensityWeighting:
+                            imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w) * image_interp[f, j, i]
+                        else:
+                            imRad[f,j,i] = _c_calculate_radiality_per_subpixel(i, j, &imGx[f,0,0], &imGy[f,0,0], xRingCoordinates, yRingCoordinates, _magnification, _ringRadius, nRingCoordinates, _radialityPositivityConstraint, h, w)
+
+        return np.asarray(imRad)
+    % endfor
+
+    
+    def _run_opencl(self, image, image_interp, magnification=5, ringRadius=0.5, border=0, radialityPositivityConstraint=True, doIntensityWeighting=True, device=None, int mem_div=1):
+
+        cl_ctx = cl.Context([device['device']])
+        cl_queue = cl.CommandQueue(cl_ctx)
+
+        cdef float _ringRadius = ringRadius * magnification
+        
+        cdef int nRingCoordinates = 12
+        cdef float angleStep = (pi * 2.) / nRingCoordinates
+        cdef float[12] xRingCoordinates, yRingCoordinates
+
+        with nogil:
+            for angleIter in range(nRingCoordinates):
+                xRingCoordinates[angleIter] = cos(angleStep * angleIter) * _ringRadius
+                yRingCoordinates[angleIter] = sin(angleStep * angleIter) * _ringRadius
+
+    
+        cdef int nFrames = image.shape[0]
+        cdef int h = image.shape[1]
+        cdef int w = image.shape[2]
+
+        cdef float[:,:,:] imGx = np.zeros(image.shape, dtype=np.float32) 
+        cdef float[:,:,:] imGy = np.zeros(image.shape, dtype=np.float32)
+        cdef float[:,:,:] image_MV = image
+        with nogil:
+            for f in range(nFrames):
+                _c_gradient_radiality(&image_MV[f,0,0], &imGx[f,0,0], &imGy[f,0,0], h, w)
+
+        image_out = np.zeros((nFrames, h*magnification, w*magnification), dtype=np.float32)
+
+        x_ring_coords = np.asarray(xRingCoordinates, dtype=np.float32)
+        y_ring_coords = np.asarray(yRingCoordinates, dtype=np.float32)
+
+        # Calculate maximum number of slices that can fit in the GPU
+        size_per_slice = 2*image[0,:,:].nbytes + image_interp[0,:,:].nbytes + imGx[0,:,:].nbytes + imGy[0,:,:].nbytes
+        max_slices = int(((x_ring_coords.nbytes + y_ring_coords.nbytes)+(device["device"].global_mem_size // size_per_slice))/mem_div)
+        max_slices = self._check_max_slices(image, max_slices)
+
+        # Initialize Buffers
+        mf = cl.mem_flags
+        image_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(image[0:max_slices,:,:].nbytes, device['device'], max_slices))
+        imageinter_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(image_interp[0:max_slices,:,:].nbytes, device['device'], max_slices))
+        imGx_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(imGx[0:max_slices,:,:].nbytes, device['device'], max_slices))
+        imGy_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(imGy[0:max_slices,:,:].nbytes, device['device'], max_slices))
+        xRingCoordinates_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(x_ring_coords.nbytes, device['device'], max_slices))
+        yRingCoordinates_in = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(y_ring_coords.nbytes, device['device'], max_slices))
+        imRad_out = cl.Buffer(cl_ctx, mf.WRITE_ONLY, self._check_max_buffer_size(image_out[0:max_slices,:,:].nbytes, device['device'], max_slices))
+
+        cl.enqueue_copy(cl_queue, image_in, image[0:max_slices,:,:]).wait()
+        cl.enqueue_copy(cl_queue, imageinter_in, image_interp[0:max_slices,:,:]).wait()
+        cl.enqueue_copy(cl_queue, imGx_in, imGx[0:max_slices,:,:]).wait()
+        cl.enqueue_copy(cl_queue, imGy_in, imGy[0:max_slices,:,:]).wait()
+        cl.enqueue_copy(cl_queue, xRingCoordinates_in, x_ring_coords).wait()
+        cl.enqueue_copy(cl_queue, yRingCoordinates_in, y_ring_coords).wait()
+
+        # Grid size
+        lowest_row = (1 + border) * magnification 
+        highest_row = (h - 1 - border) * magnification
+
+        lowest_col = (1 + border) * magnification
+        highest_col = (w - 1 - border) * magnification
+
+        code = self._get_cl_code("_le_radiality.cl", device['DP'])
+        prg = cl.Program(cl_ctx, code).build()
+        knl = prg.radiality
+
+        for i in range(0, nFrames, max_slices):
+            if nFrames - i >= max_slices:
+                n_slices = max_slices
+            else:
+                n_slices = nFrames - i
+
+            knl(
+                cl_queue,
+                (n_slices, highest_row - lowest_row, highest_col - lowest_col),
+                self.get_work_group(device['device'],(n_slices, highest_row - lowest_row, highest_col - lowest_col)),
+                image_in,
+                imageinter_in,
+                imGx_in,
+                imGy_in,
+                imRad_out,
+                xRingCoordinates_in,
+                yRingCoordinates_in,
+                np.int32(magnification),
+                np.float32(_ringRadius),
+                np.int32(nRingCoordinates),
+                np.int32(radialityPositivityConstraint),
+                np.int32(border),
+                np.int32(h),
+                np.int32(w)
+            ).wait()
+
+            cl.enqueue_copy(cl_queue, image_out[i:i+n_slices,:,:], imRad_out).wait()
+
+            if i+n_slices<image.shape[0]:
+                cl.enqueue_copy(cl_queue, image_in, image[i+n_slices:i+2*n_slices,:,:]).wait()
+                cl.enqueue_copy(cl_queue, imageinter_in, image_interp[i+n_slices:i+2*n_slices,:,:]).wait()
+                cl.enqueue_copy(cl_queue, imGx_in, imGx[i+n_slices:i+2*n_slices,:,:]).wait()
+                cl.enqueue_copy(cl_queue, imGy_in, imGy[i+n_slices:i+2*n_slices,:,:]).wait()
+    
+            cl_queue.finish()        
+        
+        image_in.release()
+        imageinter_in.release()
+        imGx_in.release()
+        imGy_in.release()
+        xRingCoordinates_in.release()
+        yRingCoordinates_in.release()
+        imRad_out.release()
+
+        return image_out
+    
---
+++ src/mako_templates/nanopyx.core.transform._le_roberts_cross_gradients.pyx
@@ -0,0 +1,126 @@
+<%!
+schedulers = ['threaded','threaded_guided','threaded_dynamic','threaded_static']
+%># cython: infer_types=True, wraparound=False, nonecheck=False, boundscheck=False, cdivision=True, language_level=3, profile=False, autogen_pxd=False
+
+import numpy as np
+cimport numpy as np
+from ...__opencl__ import cl, cl_array
+from ...__liquid_engine__ import LiquidEngine
+
+from cython.parallel import prange
+from .__interpolation_tools__ import check_image
+
+cdef extern from "_c_gradients.h":
+    void _c_gradient_roberts_cross(float* pixels, float* GxArray, float* GyArray, int w, int h) nogil
+
+class GradientRobertsCross(LiquidEngine):
+
+    def __init__(self, clear_benchmarks=False, testing=False):
+        self._designation = "GradientRobertsCross"
+        super().__init__(clear_benchmarks=clear_benchmarks, testing=testing,
+                        unthreaded_=True, threaded_=True, threaded_static_=True, 
+                        threaded_dynamic_=True, threaded_guided_=True, opencl_=True)
+
+        self._default_benchmarks = {'OpenCL': {"(['shape(100, 100, 100)'], {})": [1000000, 0.09102950000033161, 0.013413084000148956, 0.013892708000184939], "(['shape(500, 500, 500)'], {})": [125000000, 0.4193332919999193, 0.36434012500012614, 0.3667828749998989]}, 'Threaded': {"(['shape(100, 100, 100)'], {})": [1000000, 0.001157208999757131, 0.0011499580000418064, 0.001164416999927198], "(['shape(500, 500, 500)'], {})": [125000000, 0.1371792079999068, 0.12759208300030878, 0.1256785000000491]}, 'Threaded_dynamic': {"(['shape(100, 100, 100)'], {})": [1000000, 0.0009977079998861882, 0.0009290419998251309, 0.0009676250001575681], "(['shape(500, 500, 500)'], {})": [125000000, 0.11562845800017385, 0.12086337500022637, 0.11272079200034568]}, 'Threaded_guided': {"(['shape(100, 100, 100)'], {})": [1000000, 0.0009116250002989545, 0.0009819160000006377, 0.000968583000030776], "(['shape(500, 500, 500)'], {})": [125000000, 0.11294766700029868, 0.11097495800004253, 0.10783799999990151]}, 'Threaded_static': {"(['shape(100, 100, 100)'], {})": [1000000, 0.000992541999949026, 0.0010514160003367579, 0.0011669999998957792], "(['shape(500, 500, 500)'], {})": [125000000, 0.12436966700033736, 0.11687999999958265, 0.10908783299964853]}, 'Unthreaded': {"(['shape(100, 100, 100)'], {})": [1000000, 0.0016904580002119474, 0.001859041999978217, 0.0016570830002820003], "(['shape(500, 500, 500)'], {})": [125000000, 0.22857279200025005, 0.22197804200004612, 0.21746020799992039]}}
+
+    def run(self, image, run_type = None):
+        image = check_image(image)
+        return self._run(image, run_type=run_type)
+    
+    def benchmark(self, image):
+        image = check_image(image)
+        return super().benchmark(image)
+    
+    
+    # tag-start: _le_roberts_cross_gradients.GradientRobertsCross._run_unthreaded
+    def _run_unthreaded(self, float[:,:,:] image):
+
+        cdef int nFrames = image.shape[0]
+        cdef float [:,:,:] gradient_col = np.zeros_like(image) 
+        cdef float [:,:,:] gradient_row = np.zeros_like(image)
+
+        cdef int n
+        with nogil: 
+            for n in range(nFrames):
+                _c_gradient_roberts_cross(&image[n,0,0], &gradient_col[n,0,0], &gradient_row[n,0,0], image.shape[1], image.shape[2])
+        
+        return gradient_col, gradient_row
+    
+    % for sch in schedulers:
+    def _run_threaded(self, float[:,:,:] image):
+
+        cdef int nFrames = image.shape[0]
+        cdef float [:,:,:] gradient_col = np.zeros_like(image) 
+        cdef float [:,:,:] gradient_row = np.zeros_like(image)
+
+        cdef int n
+        with nogil:
+            % if sch=='threaded':
+            for n in prange(nFrames):
+            % else:
+            for n in prange(nFrames, schedule="${sch.split('_')[1]}"):
+            % endif
+                _c_gradient_roberts_cross(&image[n,0,0], &gradient_col[n,0,0], &gradient_row[n,0,0], image.shape[1], image.shape[2])
+        
+        return gradient_col, gradient_row
+    % endfor
+
+    def _run_opencl(self, float[:,:,:] image, dict device, int mem_div=1):
+
+        # QUEUE AND CONTEXT
+        cl_ctx = cl.Context([device['device']])
+        dc = device['device']
+        cl_queue = cl.CommandQueue(cl_ctx)
+
+        # Swap row and columns because opencl is strange and stores the
+        # array in a buffer in fortran ordering despite the original
+        # numpy array being in C order.
+        #image = np.ascontiguousarray(np.swapaxes(image, 1, 2), dtype=np.float32)
+
+        cdef int nFrames = image.shape[0]
+        cdef int row = image.shape[1]
+        cdef int col = image.shape[2]
+        cdef float [:,:,:] gradient_col = np.zeros_like(image) 
+        cdef float [:,:,:] gradient_row = np.zeros_like(image)
+
+        max_slices = int((dc.global_mem_size // (image[0,:,:].nbytes + gradient_col[0,:,:].nbytes + gradient_row[0,:,:].nbytes))/mem_div)
+        max_slices = self._check_max_slices(image, max_slices) 
+
+        mf = cl.mem_flags
+
+        input_opencl = cl.Buffer(cl_ctx, mf.READ_ONLY, self._check_max_buffer_size(image[0:max_slices,:,:].nbytes, device['device'], max_slices))
+        output_opencl_col = cl.Buffer(cl_ctx, mf.WRITE_ONLY, self._check_max_buffer_size(gradient_col[0:max_slices,:,:].nbytes, device['device'] , max_slices))
+        output_opencl_row = cl.Buffer(cl_ctx, mf.WRITE_ONLY, self._check_max_buffer_size(gradient_row[0:max_slices, :, :].nbytes, device['device'], max_slices))
+
+        cl.enqueue_copy(cl_queue, input_opencl, image[0:max_slices,:,:]).wait()
+
+        code = self._get_cl_code("_le_roberts_cross_gradients.cl", device['DP'])
+        prg = cl.Program(cl_ctx, code).build()
+        knl = prg.gradient_roberts_cross
+
+        for i in range(0, image.shape[0], max_slices):
+            if image.shape[0] - i >= max_slices:
+                n_slices = max_slices
+            else:
+                n_slices = image.shape[0] - i
+            knl(cl_queue,
+                (n_slices,), 
+                None, 
+                input_opencl, 
+                output_opencl_col,
+                output_opencl_row, 
+                np.int32(row), 
+                np.int32(col)).wait() 
+
+            cl.enqueue_copy(cl_queue, gradient_col[i:i+n_slices,:,:], output_opencl_col).wait()
+            cl.enqueue_copy(cl_queue, gradient_row[i:i+n_slices,:,:], output_opencl_row).wait()  
+            if i+n_slices<image.shape[0]:
+                cl.enqueue_copy(cl_queue, input_opencl, image[i+n_slices:i+2*n_slices,:,:]).wait() 
+
+            cl_queue.finish()
+
+        input_opencl.release()
+        output_opencl_col.release()
+        output_opencl_row.release()
+
+        return gradient_col, gradient_row
