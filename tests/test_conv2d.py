130cf3bdb2e8c2b2724d4e8319bf06ef11e7b222
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-01-23 15:05:58 +0000

    removed slow test

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -8,10 +8,3 @@ def test_conv_small():
     kernel = np.ones((23, 23)).astype(np.float32)
     conv = Convolution()
     conv.benchmark(small, kernel)
-
-
-def test_conv_big():
-    big = np.random.random((1000, 1000)).astype(np.float32)
-    kernel = np.ones((23, 23)).astype(np.float32)
-    conv = Convolution()
-    conv.benchmark(big, kernel)
