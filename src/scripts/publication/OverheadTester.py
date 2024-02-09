ba1fd2f78c8d1966855a616e92e08b82af357943
António Brito <50997716+antmsbrito@users.noreply.github.com>
2024-02-09 14:23:14 +0000

    Updating overhead notebook

--- src/scripts/publication/OverheadTester.py
+++ src/scripts/publication/OverheadTester.py
@@ -1,42 +1,31 @@
 import numpy as np
 
+from time import sleep
+
 from nanopyx.__liquid_engine__ import LiquidEngine
 
-class OverheadedTester(LiquidEngine):
+class OverheadTester(LiquidEngine):
 
     def __init__(self, clear_benchmarks=False, testing=False):
         self._designation = "OverheadTester"
-        super().__init__(clear_benchmarks=clear_benchmarks, testing=testing, python_=True)
+        super().__init__(clear_benchmarks=clear_benchmarks, testing=testing,
+                        opencl_=True, unthreaded_=True, threaded_=True)
 
-    def run(self,image,kernel,run_type=None):
-        self._run(image,kernel,run_type=run_type)
-        return 
-    
-    def benchmark(self,image,kernel):
-        return super().benchmark(image,kernel)
-    
-    def _run_python(self, image, kernel):
-        nRows = image.shape[0]
-        nCols = image.shape[1]
+        
+    def run(self, t, run_type=None):
+        return self._run(t, run_type=run_type)
 
-        nRows_kernel = kernel.shape[0]
-        nCols_kernel = kernel.shape[1]
+    def benchmark(self,t):
+        return super().benchmark(t)
 
-        center_r = (nRows_kernel-1) // 2
-        center_c = (nCols_kernel-1) // 2
+    def _run_opencl(self, t, device):
+        sleep(t)
+        return None
 
-        acc = 0.0
+    def _run_unthreaded(self,t):
+        sleep(1)
+        return None
 
-        conv_out = np.zeros((nRows, nCols), dtype=np.float32)
-
-        for r in range(nRows):
-            for c in range(nCols):
-                acc = 0
-                for kr in range(nRows_kernel):
-                    for kc in range(nCols_kernel):
-                        local_row = min(max(r+(kr-center_r), 0), nRows-1)
-                        local_col = min(max(c+(kc-center_c), 0), nCols-1)
-                        acc = acc + kernel[kr, kc] * image[local_row, local_col]
-                conv_out[r, c] = acc
-
-        return conv_out
+    def _run_threaded(self,t):
+        sleep(0.5)
+        return None
