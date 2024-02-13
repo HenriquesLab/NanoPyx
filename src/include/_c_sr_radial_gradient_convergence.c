f6c753c5ed02f17784ed71b6b22789c40355a743
agvesga <72971822+agvesga@users.noreply.github.com>
2024-02-13 16:01:31 +0000

    fixed RGC computation

--- src/include/_c_sr_radial_gradient_convergence.c
+++ src/include/_c_sr_radial_gradient_convergence.c
@@ -172,7 +172,8 @@ float _c_calculate_rgc3D(int xM, int yM, int sliceM, float* imIntGx, float* imIn
                                 if (GdotR < 0) {
                                     Dk = _c_calculate_dk3D(Gx, Gy, Gz, dx, dy, dz, distance);
                                     // RGC += Dk * (distanceWeightSum_xy + distanceWeightSum_z) / 2;
-                                    RGC += (Dk * distanceWeightSum);
+                                    // RGC += (Dk * distanceWeightSum); // test
+                                    RGC += (Dk * distanceWeight);
                                 }
                             }
                         }
