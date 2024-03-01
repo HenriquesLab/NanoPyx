26fcc4940eef209d22bcc1444b6e899ed9c972b4
agvesga <72971822+agvesga@users.noreply.github.com>
2024-03-01 20:58:09 +0000

    added 1D interpolation

--- src/nanopyx/core/transform/_interpolation.pyx
+++ src/nanopyx/core/transform/_interpolation.pyx
@@ -29,3 +29,28 @@ def interpolate_3d(image, magnification_xy: int = 5, magnification_z: int = 5):
     xz_interpolated = np.transpose(interpolator.run(np.transpose(xy_interpolated, axes=[2, 1, 0]).copy(), 0, 0, 1, magnification_z), axes=[2,1,0]).copy()
 
     return np.mean(np.array([yz_interpolated, xz_interpolated]), axis=0)
+
+def linear_interpolation_1D_z(image, magnification):
+    # linear interpolator that takes a 3D image and do linear interpolation in a 1D column in the chosen axis
+
+    image_interpolated = np.zeros((image.shape[0] * magnification, image.shape[1], image.shape[2]), dtype=np.float32)
+
+    # for s in range(image.shape[0]):
+    #     for r in range(image.shape[1]):
+    #         for c in range(image.shape[2]):
+    #             image_interpolated[s*magnification:(s+1)*magnification, r, c] = np.linspace(image[s, r, c], image[s+1, r, c], magnification)
+
+    # run over all the planes
+    for s in range(image.shape[0]-1):
+        image_interpolated[s*magnification:(s+1)*magnification, :,:] = np.linspace(image[s, :,:], image[s+1, :,:], magnification)
+
+    return image_interpolated
+
+
+def interpolate_3d_zlinear(image, magnification_xy: int = 5, magnification_z: int = 5):
+    interpolator_xy = ShiftMagnify_CR()
+
+    xy_interpolated = interpolator_xy.run(image, 0, 0, magnification_xy, magnification_xy)
+    z_interpolated = linear_interpolation_1D_z(xy_interpolated, magnification_z)
+
+    return z_interpolated
