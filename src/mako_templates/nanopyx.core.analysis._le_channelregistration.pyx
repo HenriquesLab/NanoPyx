06c84fb4abc354f37080c1e7cf258e5fc2ac65fd
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-03-01 16:43:13 +0000

    added opencl device detection and implementation

--- src/mako_templates/nanopyx.core.analysis._le_channelregistration.pyx
+++ src/mako_templates/nanopyx.core.analysis._le_channelregistration.pyx
@@ -15,6 +15,7 @@ from .ccm cimport _calculate_slice_ccm
 from .estimate_shift import GetMaxOptimizer
 from ..transform._le_interpolation_bicubic import ShiftAndMagnify
 from ..transform import Convolution2D
+from ..utils.cl_device import get_fastest_device_name
 
 cdef bint _check_even_square(float[:, :] image_arr) nogil:
     cdef int r = image_arr.shape[0]
@@ -75,7 +76,7 @@ cdef float[:, :] _calculate_ccm_from_ref(float[:, :] img_stack, float[:, :] img_
 
     return ccm
 
-def _gaussian_filter(inpimg, sigma):
+def _gaussian_filter(inpimg, _runtype, sigma):
     
     conv = Convolution2D(verbose=True) 
 
@@ -85,8 +86,8 @@ def _gaussian_filter(inpimg, sigma):
     knl1 = knl1 / knl1.sum()
     knl1 = knl1.astype(np.float32)
 
-    img1 = conv.run(inpimg,knl1[:,np.newaxis],run_type="Threaded")
-    img2 = conv.run(img1,knl1[np.newaxis,:],run_type="Threaded")
+    img1 = conv.run(inpimg,knl1.reshape((len(x1), 1)),run_type=_runtype)
+    img2 = conv.run(img1,knl1.reshape((1, len(x1))),run_type=_runtype)
 
     return img2
 
@@ -99,7 +100,7 @@ class ChannelRegistrationEstimator(LiquidEngine):
         self._designation = "ChannelRegistrationEstimator"
         super().__init__(
             clear_benchmarks=clear_benchmarks, testing=testing, 
-            unthreaded_=True, threaded_=True, threaded_static_=True, threaded_dynamic_=True, threaded_guided_=True, verbose=verbose)
+            unthreaded_=True, threaded_=True, threaded_static_=True, threaded_dynamic_=True, threaded_guided_=True, opencl_=True, verbose=verbose)
         
     def run(self, img_stack, img_ref, max_shift, blocks_per_axis, min_similarity, run_type=None):
         return self._run(img_stack, img_ref, max_shift, blocks_per_axis, min_similarity, run_type=run_type)
@@ -109,7 +110,7 @@ class ChannelRegistrationEstimator(LiquidEngine):
 
     % for sch in schedulers:
     def _run_${sch}(self, float[:,:,:] img_stack, float[:,:] img_ref, int max_shift, int blocks_per_axis, float min_similarity):
-
+        _runtype = "${sch}".capitalize()
         crsm = ShiftAndMagnify(verbose=False)
         
         cdef int nChannels =  img_stack.shape[0]    
@@ -165,7 +166,7 @@ class ChannelRegistrationEstimator(LiquidEngine):
                         slice_ccm = slice_ccm[ccm_row_start:ccm_row_start+(max_shift*2), ccm_col_start:ccm_col_start + (max_shift * 2)]
 
                     # Upscale 10x around max_idx                     
-                    upscaled_ccm_slice = crsm.run(np.ascontiguousarray(slice_ccm),0,0,10,10,run_type='Threaded')[0]                    
+                    upscaled_ccm_slice = crsm.run(np.ascontiguousarray(slice_ccm),0,0,10,10,run_type=_runtype)[0]                    
                     max_idx_upscaled = np.unravel_index(np.argmax(upscaled_ccm_slice), np.array(upscaled_ccm_slice).shape)
                     max_coords = np.asarray(max_idx_upscaled).astype(np.float32) // 10
                     ccm_max_value = upscaled_ccm_slice[max_idx_upscaled[0],max_idx_upscaled[1]]
@@ -235,8 +236,8 @@ class ChannelRegistrationEstimator(LiquidEngine):
 
             if blocks_per_axis > 1:
 
-                _translation_matrix_c = _gaussian_filter(np.array(_translation_matrix_c), sigma=max(block_nCols, block_nRows) / 2.0)
-                _translation_matrix_r = _gaussian_filter(np.array(_translation_matrix_r), sigma=max(block_nCols, block_nRows) / 2.0)
+                _translation_matrix_c = _gaussian_filter(np.array(_translation_matrix_c), _runtype, sigma=max(block_nCols, block_nRows) / 2.0)
+                _translation_matrix_r = _gaussian_filter(np.array(_translation_matrix_r), _runtype, sigma=max(block_nCols, block_nRows) / 2.0)
 
             translation_masks[channel,:,:nCols] = _translation_matrix_c
             translation_masks[channel,:, nCols:] = _translation_matrix_r
@@ -245,7 +246,128 @@ class ChannelRegistrationEstimator(LiquidEngine):
 
     % endfor
 
+    def _run_${sch}(self, float[:,:,:] img_stack, float[:,:] img_ref, int max_shift, int blocks_per_axis, float min_similarity):
+        _runtype = get_fastest_device_name()
+        crsm = ShiftAndMagnify(verbose=False)
+        
+        cdef int nChannels =  img_stack.shape[0]    
+        cdef int nRows =  img_stack.shape[1]
+        cdef int nCols = img_stack.shape[2]    
 
-# TODO REFACTOR METHODS.CHANNEL_REGISTRATION TO HOLD THE NEW CLASSES
-# TODO TRY TO PARALLELIZE THE SECOND LOOP?!?!?!? why is it not
-# TODO OPTIMIZE ESTIMATOR 
+        cdef int block_nRows = nRows // blocks_per_axis
+        cdef int block_nCols = nCols // blocks_per_axis
+
+        cdef float[:,:,:] translation_masks = np.zeros((nChannels, nRows, nCols * 2)).astype(np.float32)
+    
+        cdef int channel, row_i, col_i
+        cdef int row_start, col_start
+        cdef int ccm_col_start, ccm_row_start
+        cdef float[:,:] img_slice 
+        cdef float[:,:] slice_crop, ref_crop, slice_ccm
+
+        cdef float[:,:] flow_arrows
+        cdef int n_flow_arrows = 0
+
+        cdef float[:] max_coords
+        cdef float[:,:] upscaled_ccm_slice
+        cdef float vector_c, vector_r, ccm_max_value
+
+        cdef int ccm_cols, ccm_rows
+        translation_matrix_c = np.zeros((nRows, nCols)).astype(np.float32)
+        translation_matrix_r = np.zeros((nRows, nCols)).astype(np.float32)
+        cdef float[:,:] _translation_matrix_c = translation_matrix_c
+        cdef float[:,:] _translation_matrix_r = translation_matrix_r
+
+        cdef int j,i,n_arrow,idx
+        cdef float dx,dy,w_sum,max_distance,all_distances
+        cdef float[:,:,:] distances
+
+        cdef float d, first_term, second_term, weight
+
+        for channel in range(nChannels):
+            img_slice = img_stack[channel,:,:]
+            n_flow_arrows = 0
+            flow_arrows = np.zeros((blocks_per_axis**2,4)).astype(np.float32)
+            for row_i in range(blocks_per_axis):
+                for col_i in range(blocks_per_axis):
+                    row_start = row_i * block_nRows
+                    col_start = col_i * block_nCols
+
+                    slice_crop = img_slice[row_start:row_start+block_nRows, col_start:col_start+block_nCols]
+                    ref_crop = img_ref[row_start:row_start+block_nRows, col_start:col_start+block_nCols]
+                    slice_ccm = _calculate_ccm_from_ref(slice_crop, ref_crop)
+
+                    if max_shift > 0 and max_shift * 2 + 1 < slice_ccm.shape[0] and max_shift * 2 + 1 < slice_ccm.shape[1]:
+                        ccm_col_start = slice_ccm.shape[1] // 2 - max_shift
+                        ccm_row_start = slice_ccm.shape[0] // 2 - max_shift
+                        slice_ccm = slice_ccm[ccm_row_start:ccm_row_start+(max_shift*2), ccm_col_start:ccm_col_start + (max_shift * 2)]
+
+                    # Upscale 10x around max_idx                     
+                    upscaled_ccm_slice = crsm.run(np.ascontiguousarray(slice_ccm),0,0,10,10,run_type=_runtype)[0]                    
+                    max_idx_upscaled = np.unravel_index(np.argmax(upscaled_ccm_slice), np.array(upscaled_ccm_slice).shape)
+                    max_coords = np.asarray(max_idx_upscaled).astype(np.float32) // 10
+                    ccm_max_value = upscaled_ccm_slice[max_idx_upscaled[0],max_idx_upscaled[1]]
+
+                    ccm_cols = slice_ccm.shape[1]
+                    ccm_rows = slice_ccm.shape[0]
+                    if ccm_max_value >= min_similarity:
+                        vector_c = ccm_cols / 2.0 - max_coords[1] - 1 
+                        vector_r = ccm_rows / 2.0 - max_coords[0] - 1
+                        flow_arrows[n_flow_arrows,0] = col_start+block_nCols/2.0
+                        flow_arrows[n_flow_arrows,1] = row_start+block_nRows/2.0
+                        flow_arrows[n_flow_arrows,2] = vector_c
+                        flow_arrows[n_flow_arrows,3] = vector_r
+                        n_flow_arrows += 1
+
+            if n_flow_arrows==0:
+                return None
+
+            translation_matrix_r = np.zeros((nRows, nCols)).astype(np.float32)
+            translation_matrix_c = np.zeros((nRows, nCols)).astype(np.float32)
+
+            max_distance = sqrt(nRows * nRows + nCols * nCols)
+            distances = np.zeros((n_flow_arrows,nRows,nCols)).astype(np.float32)
+
+            with nogil:
+                for j in prange(nRows):
+                    for i in range(nCols):
+                        # iterate over vectors
+                        dx = 0
+                        dy = 0 
+                        w_sum = 0
+
+                        if n_flow_arrows == 1:
+                            dx = flow_arrows[0,2]
+                            dy = flow_arrows[0,3]
+                        else:
+                            all_distances = 0
+                            for n_arrow in range(n_flow_arrows):
+                                d = sqrt(pow(flow_arrows[n_arrow,0] - i, 2) + pow(flow_arrows[n_arrow,1] - j, 2)) + 1
+                                distances[n_arrow,j,i] = d
+                                all_distances = all_distances + pow(((max_distance - d) / (max_distance * d)), 2)
+
+                            for idx in range(n_flow_arrows):
+                                d = distances[idx,j,i]
+                                first_term = pow(((max_distance - d) / (max_distance * d)), 2)
+                                second_term = all_distances
+
+                                weight = first_term / second_term
+                                dx = dx + flow_arrows[idx,2] * weight
+                                dy = dy + flow_arrows[idx,3] * weight
+                                w_sum = w_sum + weight
+
+                            dx = dx / w_sum
+                            dy = dy / w_sum
+
+                        _translation_matrix_c[j, i] = dx
+                        _translation_matrix_r[j, i] = dy
+
+            if blocks_per_axis > 1:
+
+                _translation_matrix_c = _gaussian_filter(np.array(_translation_matrix_c), _runtype, sigma=max(block_nCols, block_nRows) / 2.0)
+                _translation_matrix_r = _gaussian_filter(np.array(_translation_matrix_r), _runtype, sigma=max(block_nCols, block_nRows) / 2.0)
+
+            translation_masks[channel,:,:nCols] = _translation_matrix_c
+            translation_masks[channel,:, nCols:] = _translation_matrix_r
+
+        return np.array(translation_masks)
