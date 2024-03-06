86d0e55ba6e61a8577aab82f2b68fe9cfe42778a
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-03-06 19:22:18 +0000

    reenabling a single test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -278,40 +278,40 @@
 # # tag-end
 
 
-# # tag-copy: test_interpolation_nearest_neighbor_ShiftScaleRotate; replace("nearest_neighbor", "lanzcos"); replace("NNShiftScaleRotate", "LZShiftScaleRotate")
-# def test_interpolation_lanzcos_ShiftScaleRotate(): #(plt):
-#     M = 4
-#     nFrames = 3
-#     image = get_simplex_noise(64, 32, frames=nFrames, amplitude=1000)
-#     shift_row = 5.0
-#     shift_col = 5.0
-#     angle = np.pi / 4
-#     SM = LZShiftScaleRotate(testing=True,clear_benchmarks=True)
-#     bench_values = SM.benchmark(image, shift_row, shift_col, M, M, angle)
+# tag-copy: test_interpolation_nearest_neighbor_ShiftScaleRotate; replace("nearest_neighbor", "lanzcos"); replace("NNShiftScaleRotate", "LZShiftScaleRotate")
+def test_interpolation_lanzcos_ShiftScaleRotate(): #(plt):
+    M = 4
+    nFrames = 3
+    image = get_simplex_noise(64, 32, frames=nFrames, amplitude=1000)
+    shift_row = 5.0
+    shift_col = 5.0
+    angle = np.pi / 4
+    SM = LZShiftScaleRotate(testing=True,clear_benchmarks=True)
+    bench_values = SM.benchmark(image, shift_row, shift_col, M, M, angle)
 
-#     # images = []
-#     # titles = []
-#     # run_times = []
+    # images = []
+    # titles = []
+    # run_times = []
 
-#     # # unzip the values
-#     # for run_time, title, image in bench_values:
-#     #     run_times.append(run_time)  
-#     #     titles.append(title)
-#     #     images.append(image)
+    # # unzip the values
+    # for run_time, title, image in bench_values:
+    #     run_times.append(run_time)  
+    #     titles.append(title)
+    #     images.append(image)
 
-#     # # ensure images are similar
-#     # for i in range(len(images)):
-#     #     for j in range(i + 1, len(images)):
-#     #         np.testing.assert_allclose(images[i], images[j], rtol=1e1)
+    # # ensure images are similar
+    # for i in range(len(images)):
+    #     for j in range(i + 1, len(images)):
+    #         np.testing.assert_allclose(images[i], images[j], rtol=1e1)
 
-#     # show images
-#     # fig, axes = plt.subplots(nFrames, len(images), figsize=(20, 10))
-#     # for i in range(nFrames):
-#     #     for j in range(len(images)):
-#     #         if i == 0:
-#     #             axes[i, j].set_title(titles[j])
-#     #         axes[i, j].imshow(images[j][i], cmap="hot")
-#     #         axes[i, j].axis("off")
+    # show images
+    # fig, axes = plt.subplots(nFrames, len(images), figsize=(20, 10))
+    # for i in range(nFrames):
+    #     for j in range(len(images)):
+    #         if i == 0:
+    #             axes[i, j].set_title(titles[j])
+    #         axes[i, j].imshow(images[j][i], cmap="hot")
+    #         axes[i, j].axis("off")
 
 
-# # tag-end
+# tag-end
