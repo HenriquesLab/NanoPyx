f33d70784e5860791308b78ddb720c490a6f272c
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-03-06 17:24:00 +0000

    disabling tests

--- tests/test_example_data_download.py
+++ tests/test_example_data_download.py
@@ -1,27 +1,27 @@
-# from nanopyx.data.download import ExampleDataManager
-# from nanopyx.core.io.zip_image_loader import ZipTiffIterator
+from nanopyx.data.download import ExampleDataManager
+from nanopyx.core.io.zip_image_loader import ZipTiffIterator
 
 
-# def test_list_datasets(downloader: ExampleDataManager):
-#     datasets = downloader.list_datasets()
-#     assert isinstance(datasets, list)
-#     assert all(isinstance(d, str) for d in datasets)
+def test_list_datasets(downloader: ExampleDataManager):
+    datasets = downloader.list_datasets()
+    assert isinstance(datasets, list)
+    assert all(isinstance(d, str) for d in datasets)
 
 
-# def test_list_datasets_nickname(downloader: ExampleDataManager):
-#     datasets = downloader.list_datasets_nickname()
-#     assert isinstance(datasets, list)
-#     assert all(isinstance(d, tuple) and len(d) == 2 for d in datasets)
-#     assert all(isinstance(d[0], str) and isinstance(d[1], str) for d in datasets)
+def test_list_datasets_nickname(downloader: ExampleDataManager):
+    datasets = downloader.list_datasets_nickname()
+    assert isinstance(datasets, list)
+    assert all(isinstance(d, tuple) and len(d) == 2 for d in datasets)
+    assert all(isinstance(d[0], str) and isinstance(d[1], str) for d in datasets)
 
 
-# def test_get_dataset_info(downloader: ExampleDataManager):
-#     dataset_info = downloader.get_dataset_info("SMLMS2013_HDTubulinAlexa647")
-#     assert isinstance(dataset_info, dict)
+def test_get_dataset_info(downloader: ExampleDataManager):
+    dataset_info = downloader.get_dataset_info("SMLMS2013_HDTubulinAlexa647")
+    assert isinstance(dataset_info, dict)
 
 
-# def test_load_dataset(downloader: ExampleDataManager):
-#     tiff_iterator = downloader.get_ZipTiffIterator(
-#         "SMLMS2013_HDTubulinAlexa647", as_ndarray=False
-#     )
-#     assert isinstance(tiff_iterator, ZipTiffIterator)
+def test_load_dataset(downloader: ExampleDataManager):
+    tiff_iterator = downloader.get_ZipTiffIterator(
+        "SMLMS2013_HDTubulinAlexa647", as_ndarray=False
+    )
+    assert isinstance(tiff_iterator, ZipTiffIterator)
