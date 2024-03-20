97efe4869b999a6a517405c4e67496a764ff978c
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-03-20 15:11:30 +0000

    added relative imports to methods level

--- src/nanopyx/methods/__init__.py
+++ src/nanopyx/methods/__init__.py
@@ -1,5 +1,9 @@
 from . import drift_alignment
 from . import channel_registration
 
+from .restoration import non_local_means_denoising
 from .esrrf.eSRRF_workflow import eSRRF
 from .srrf.SRRF_workflow import SRRF
+from .squirrel import calculate_frc, calculate_decorr_analysis, calculate_error_map
+from .drift_alignment import estimate_drift_alignment, apply_drift_alignment
+from .channel_registration import estimate_channel_registration, apply_channel_registration
