b31cd995fd9015f2082fc69248dc9152bc28557c
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-03-20 15:55:52 +0000

    high-level param sweep function

---
+++ src/nanopyx/methods/esrrf/parameter_sweep.py
@@ -0,0 +1,32 @@
+import numpy as np
+from ...core.analysis.parameter_sweep import ParameterSweep
+
+
+def run_esrrf_parameter_sweep(img: np.ndarray, magnification: int = 2, sensitivities: list = [1, 2], radii: list = [1, 1.5], temporal_correlation: str = "AVG"):
+    """
+    Conducts a parameter sweep for enhanced Super-Resolution Radial Fluctuations (eSRRF) analysis on an image.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        The input image as a 2D numpy array.
+    magnification : int, optional
+        The magnification factor to be applied during the ESRRF analysis. Default is 2.
+    sensitivities : list of int, optional
+        A list of sensitivity values to be used in the parameter sweep. Default is [1, 2].
+    radii : list of float, optional
+        A list of radii values to be used in the parameter sweep. Default is [1, 1.5].
+    temporal_correlation : str, optional
+        The method of temporal correlation to be used. Default is "AVG".
+
+    Returns
+    -------
+    np.ndarray
+        An array with QnR value for each combination of sensitivities and radii.
+
+    Notes
+    -----
+    This function performs a parameter sweep for the ESRRF method, which is used for super-resolution imaging analysis. It varies sensitivity and radius to find optimal settings for image enhancement.
+    """
+    ps = ParameterSweep()
+    return ps.run(img, magnification, sensitivity_array=sensitivities, radius_array=radii, temporal_correlation=temporal_correlation)
