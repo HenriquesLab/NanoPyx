b2d235679afcb5e4c71ed331adc0b855aacaf7b6
agvesga <72971822+agvesga@users.noreply.github.com>
2024-03-22 14:41:24 +0000

    gradient declaration improve

--- src/nanopyx/core/transform/_le_esrrf3d.pyx
+++ src/nanopyx/core/transform/_le_esrrf3d.pyx
@@ -174,20 +174,22 @@ class eSRRF3D(LiquidEngine):
             # image_interpolated = interpolator.run(image[f], 0, 0, _magnification_xy, _magnification_xy)
 
             # image_interpolated = interpolate_3d(image[f], _magnification_xy, _magnification_z)
-            image_interpolated = interpolate_3d_zlinear(image[f], _magnification_xy, _magnification_z)
+            image_interpolated = interpolate_3d_zlinear(image[f,:,:,:], _magnification_xy, _magnification_z)
 
             n_slices_mag, n_rows_mag, n_cols_mag = image_interpolated.shape[0], image_interpolated.shape[1], image_interpolated.shape[2]
 
-            gradients_c = np.zeros((n_slices, n_rows*Gx_Gy_MAGNIFICATION, n_cols*Gx_Gy_MAGNIFICATION), dtype=np.float32)
-            gradients_r = np.zeros((n_slices, n_rows*Gx_Gy_MAGNIFICATION, n_cols*Gx_Gy_MAGNIFICATION), dtype=np.float32)
-            gradients_s = np.zeros((n_slices, n_rows*Gx_Gy_MAGNIFICATION, n_cols*Gx_Gy_MAGNIFICATION), dtype=np.float32)
-
             img_dum = interpolator.run(image[f], 0, 0, Gx_Gy_MAGNIFICATION, Gx_Gy_MAGNIFICATION)
             # img_dum = interpolate_3d(image[f], Gx_Gy_MAGNIFICATION, _magnification_z)
+            # img_dum = interpolate_3d_zlinear(image[f], Gx_Gy_MAGNIFICATION, Gx_Gy_MAGNIFICATION)
+            
             n_slices_mag_dum, n_rows_mag_dum, n_cols_mag_dum = img_dum.shape[0], img_dum.shape[1], img_dum.shape[2]
 
+            gradients_c = np.zeros((n_slices_mag_dum, n_rows_mag_dum, n_cols_mag_dum), dtype=np.float32)
+            gradients_r = np.zeros((n_slices_mag_dum, n_rows_mag_dum, n_cols_mag_dum), dtype=np.float32)
+            gradients_s = np.zeros((n_slices_mag_dum, n_rows_mag_dum, n_cols_mag_dum), dtype=np.float32)
+
             with nogil:
-                _c_gradient_3d(&img_dum[0, 0, 0], &gradients_c[0, 0, 0], &gradients_r[0, 0, 0], &gradients_s[0, 0, 0], n_slices, n_rows_mag_dum, n_cols_mag_dum)
+                _c_gradient_3d(&img_dum[0, 0, 0], &gradients_c[0, 0, 0], &gradients_r[0, 0, 0], &gradients_s[0, 0, 0], n_slices_mag_dum, n_rows_mag_dum, n_cols_mag_dum)
 
             # gradients_s_interpolated = interpolator.run(gradients_s, 0, 0, _magnification_xy, _magnification_xy)
             # gradients_r_interpolated = interpolator.run(gradients_r, 0, 0, _magnification_xy, _magnification_xy)
@@ -236,7 +238,7 @@ class eSRRF3D(LiquidEngine):
                                 # z0 = z0 * _magnification_z
                                 rgc_map[f, sM, rM, cM] = rgc_val
 
-        return np.asarray(rgc_map)
+        return np.asarray(rgc_map) #, np.asarray(gradients_s_interpolated), np.asarray(gradients_r_interpolated), np.asarray(image_interpolated) # debug
 
     def get_gradients(self):
         if self._gradients_c_interpolated is None or self._gradients_r_interpolated is None or self._gradients_s_interpolated is None:
