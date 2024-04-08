a5e54a492338042727ed7b8b67bab9c99d7c8f15
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-04-08 14:10:38 +0100

    changed default values to have a var that can be targeted by tag2tag

--- src/notebookchef/pantry/methods/esrrf.py
+++ src/notebookchef/pantry/methods/esrrf.py
@@ -51,10 +51,15 @@ def run_esrrf(b):
             tiff.imwrite(name + "_esrrf.tif", dataset_esrrf)
     gui_esrrf._main_display.children = gui_esrrf._main_display.children + (stackview.slice(dataset_esrrf, colormap=gui_esrrf["cmaps"].value, continuous_update=True),)
 
-gui_esrrf.add_float_slider("ring_radius", description="Ring Radius:", min=0.1, max=3.0, value=1.5, remember_value=True)
-gui_esrrf.add_int_slider("sensitivity", description="Sensitivity:", min=1, max=10, value=1)
-gui_esrrf.add_int_slider("magnification", description="Magnification:", min=1, max=10, value=5)
-gui_esrrf.add_int_slider("esrrf_order", description="eSRRF order:", min=1, max=3, value=1)
+
+default_radius = 1.5
+default_sensitivity = 1
+default_magnification = 5
+default_esrrf_order = 1
+gui_esrrf.add_float_slider("ring_radius", description="Ring Radius:", min=0.1, max=3.0, value=default_radius, remember_value=True)
+gui_esrrf.add_int_slider("sensitivity", description="Sensitivity:", min=1, max=10, value=default_sensitivity)
+gui_esrrf.add_int_slider("magnification", description="Magnification:", min=1, max=10, value=default_magnification)
+gui_esrrf.add_int_slider("esrrf_order", description="eSRRF order:", min=1, max=3, value=default_esrrf_order)
 gui_esrrf.add_label("-=-= Time-Lapse =-=-")
 gui_esrrf.add_int_slider("frames_per_timepoint", description="Frames per time-point (0 - auto)", min=0, max=dataset_original.shape[0], value=dataset_original.shape[0]//2)
 gui_esrrf.add_checkbox("save", description="Save Output", value=True)
