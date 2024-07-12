cda75dafedfd64b4e56b083173049c877b50462f
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-07-12 13:46:40 +0100

    fixing test change

--- tests/test_decorr_analysis.py
+++ tests/test_decorr_analysis.py
@@ -3,6 +3,6 @@ from nanopyx.core.analysis.decorr import DecorrAnalysis
 
 
 def test_decorr_analysis(random_timelapse_w_drift):
-    decorr = DecorrAnalysis(pixel_size=1, units="pixel", do_plot=False)
+    decorr = DecorrAnalysis(pixel_size=1, units="pixel")
     decorr.run_analysis(random_timelapse_w_drift[0])
     assert not math.isinf(decorr.resolution)
