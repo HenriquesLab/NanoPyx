bdc3070c8289497434333788afc5fcb11c603ba1
António Brito <50997716+antmsbrito@users.noreply.github.com>
2025-03-13 10:28:20 +0000

    Add another test to check if 2D convolution still works fine

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -5,14 +5,22 @@ from nanopyx.core.transform.convolution import convolution2D_numba
 
 
 def test_conv_small():
-    small = np.random.random((100, 100)).astype(np.float32)
+    small = np.random.random((10,100, 100)).astype(np.float32)
     kernel = np.ones((23, 23)).astype(np.float32)
     conv = Convolution()
     conv.benchmark(small, kernel)
 
 
 def test_convolution2D_numba():
-    img = np.random.random((100, 100)).astype(np.float32)
+    img = np.random.random((10,100, 100)).astype(np.float32)
     kernel = np.random.random((3, 3)).astype(np.float32)
 
     convolution2D_numba(img, kernel)
+
+
+def test_conv_small_2d():
+    small = np.random.random((100, 100)).astype(np.float32)
+    kernel = np.ones((23, 23)).astype(np.float32)
+    conv = Convolution()
+    conv.benchmark(small, kernel)
+
