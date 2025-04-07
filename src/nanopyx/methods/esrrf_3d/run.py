2c5b4f1fbb9a255f702ca71f55cd761c002c9138
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-04-07 14:48:25 +0100

    udpated run function

--- src/nanopyx/methods/esrrf_3d/run.py
+++ src/nanopyx/methods/esrrf_3d/run.py
@@ -1,27 +1,41 @@
 from ...core.transform._le_esrrf3d import eSRRF3D
-from ...core.transform.sr_temporal_correlations import calculate_eSRRF3d_temporal_correlations
+from ...core.transform.sr_temporal_correlations import (
+    calculate_eSRRF3d_temporal_correlations,
+)
 
-def run_esrrf3d(img,correlation="AVG", framewindow=5, rollingoverlap=2, **kwargs):
+
+def run_esrrf3d(
+    img,
+    mode="average",
+    magnification_xy=2,
+    magnification_z=2,
+    radius=1.5,
+    sensitivity=1,
+    voxel_ratio=4,
+    doIntensityWeighting=True,
+    **kwargs,
+):
     """
-    Calculate the eSRRF3D temporal correlations for the given image.
+    Calculate the eSRRF3D temporal correlations for the given 3D image.
 
-    Args:
-        img: The input 3D image.
-        magnification_xy: The magnification factor for the x and y axes.
-        magnification_z: The magnification factor for the z axis.
-        radius: The radius for the xy plane.
-        radius_z: The radius for the z axis.
-        sensitivity: The sensitivity for the calculation.
-        doIntensityWeighting: Whether to perform intensity weighting.
-        run_type: The type of the run.
-        keep_gradients: Whether to keep the gradients.
-        keep_interpolated: Whether to keep the interpolated values.
-        correlation: The type of correlation to use.
-        framewindow: The window size for frame.
-        rollingoverlap: The overlap size for rolling.
+        img (ndarray): The input 4D image.
+        mode (str, optional): The mode of time projection, default is "average", other option is "std".
+        magnification_xy (float, optional): The magnification factor for the x and y axes. Default is 2.
+        magnification_z (float, optional): The magnification factor for the z axis. Default is 2.
+        radius (float, optional): The radius for the xy plane. Default is 1.5.
+        sensitivity (float, optional): The sensitivity for the calculation. Default is 1.
+        voxel_ratio (float, optional): The ratio of voxel dimensions (z to xy). Default is 4.
+        doIntensityWeighting (bool, optional): Whether to perform intensity weighting. Default is True.
+        **kwargs: Additional parameters for the eSRRF3D calculation, including:
+            - radius_z (float, optional): The radius for the z axis.
+            - run_type (str, optional): The type of the run.
+            - keep_gradients (bool, optional): Whether to keep the gradients.
+            - keep_interpolated (bool, optional): Whether to keep the interpolated values.
+            - correlation (str, optional): The type of correlation to use.
+            - framewindow (int, optional): The window size for frames.
+            - rollingoverlap (int, optional): The overlap size for rolling.
 
-    Returns:
-        The calculated eSRRF3D temporal correlations.
+        ndarray: The calculated eSRRF3D temporal correlations.
     """
     esrrf_calculator = eSRRF3D()
-    return calculate_eSRRF3d_temporal_correlations(esrrf_calculator.run(img, **kwargs), correlation=correlation, framewindow=framewindow, rollingoverlap=rollingoverlap)
+    return esrrf_calculator.run(img, **kwargs)
