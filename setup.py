c76d6888a4113cae25ae10dbea234a7c51d651f1
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-04-22 11:10:26 +0100

    version bum to current

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ EXTRA_COMPILE_ARGS = [
 ]
 EXTRA_LING_ARGS = []
 
-VERSION = "1.0.0"  # sets version number for whole package
+VERSION = "1.1.0"  # sets version number for whole package
 
 
 def run_command(command: str) -> str:
