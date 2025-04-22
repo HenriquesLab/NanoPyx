0aaf732ef2182c038716a3945507b7edb2701708
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-04-22 11:01:41 +0100

    added 3.13 support

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import nox
 
 DIR = Path(__file__).parent.resolve()
-PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12"]
+PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 PYTHON_DEFAULT_VERSION = "3.10"
 
 # Platform logic
