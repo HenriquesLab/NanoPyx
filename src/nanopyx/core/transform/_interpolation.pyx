a5adccf78bea6b7ca105e19af2e6d0ed0a5ffba8
Bruno Manuel Santos Saraiva <bruno.msaraiva2@gmail.com>
2025-10-21 11:32:56 +0100

    adding fht interpolation function

--- src/nanopyx/core/transform/_interpolation.pyx
+++ src/nanopyx/core/transform/_interpolation.pyx
@@ -5,6 +5,8 @@ cimport numpy as np
 
 import cython
 from math import floor
+import numpy.fft as fft
+from libc.math cimport ceil, log2, pow
 
 from ._le_interpolation_catmull_rom import ShiftAndMagnify as ShiftMagnify_CR
 
@@ -99,3 +101,119 @@ def interpolate_3d_zlinear(image, magnification_xy: int = 5, magnification_z: in
         z_interpolated = np.asarray(xy_interpolated)
 
     return z_interpolated
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef float[:, :] _mirror_padding_even_square_c(float[:, :] img):
+    cdef int h = img.shape[0]
+    cdef int w = img.shape[1]
+
+    # Get power-of-2 dimension >= each side, then pick square
+    cdef int bitW = <int>pow(2, ceil(log2(w)))
+    cdef int bitH = <int>pow(2, ceil(log2(h)))
+    cdef int outputDim = max(bitH, bitW)
+
+    cdef int scaleW = <int>ceil(outputDim / float(w))
+    if scaleW % 2 == 0:
+        scaleW += 1
+    cdef int scaleH = <int>ceil(outputDim / float(h))
+    if scaleH % 2 == 0:
+        scaleH += 1
+
+    cdef int i, j, sH, sW, p, q
+    cdef float[:, :] padded = np.zeros((scaleH * h, scaleW * w), dtype=np.float32)
+
+    # use integer arithmetic for the tile-flip decision
+    cdef int midW = (scaleW - 1) // 2
+    cdef int midH = (scaleH - 1) // 2
+
+    for j in range(h):
+        for i in range(w):
+            for sH in range(scaleH):
+                for sW in range(scaleW):
+                    if ((sW - midW) % 2) == 0:
+                        p = i
+                    else:
+                        p = w - 1 - i
+                    if ((sH - midH) % 2) == 0:
+                        q = j
+                    else:
+                        q = h - 1 - j
+                    padded[j + sH * h, i + sW * w] = img[q, p]
+
+    # Crop to square outputDim
+    cdef int xROI = (scaleW * w - outputDim) // 2
+    cdef int yROI = (scaleH * h - outputDim) // 2
+    return padded[yROI:yROI + outputDim, xROI:xROI + outputDim]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef float[:, :] _fht_space_interpolation_c(float[:, :] img, int intFactor, bint doMirrorPadding):
+    cdef float[:, :] working_img
+    cdef float orig_min = np.min(img)
+    cdef float orig_max = np.max(img)
+    cdef float scale_factor
+
+    if doMirrorPadding:
+        working_img = _mirror_padding_even_square_c(img)
+    else:
+        working_img = img
+
+    # Forward FFT (use complex128 for accuracy)
+    cdef complex[:, :] F = fft.fft2(np.asarray(working_img))
+    cdef complex[:, :] Fshift = fft.fftshift(F)
+
+    cdef int h = Fshift.shape[0]
+    cdef int w = Fshift.shape[1]
+    cdef int hInt = h * intFactor
+    cdef int wInt = w * intFactor
+
+    # Zero-padded enlarged spectrum and place centered block
+    cdef complex[:, :] FInt = np.zeros((hInt, wInt), dtype=np.complex128)
+
+    # center positions (integer)
+    cdef int xROI = (wInt - w) // 2
+    cdef int yROI = (hInt - h) // 2
+
+    # place the whole centered block at once (faster and correct)
+    FInt[yROI:yROI + h, xROI:xROI + w] = Fshift
+
+    # Inverse transform
+    cdef complex[:, :] result = fft.ifft2(fft.ifftshift(FInt))
+    cdef float[:, :] output = np.real(result).astype(np.float32)
+
+    # Scale to match input range (robust to constant images)
+    cdef float out_min = np.min(output)
+    cdef float out_max = np.max(output)
+    if out_max != out_min:
+        scale_factor = (orig_max - orig_min) / (out_max - out_min)
+        output = (np.asarray(output) - out_min) * scale_factor + orig_min
+
+    if doMirrorPadding:
+        xROI = (wInt - intFactor * img.shape[1]) // 2
+        yROI = (hInt - intFactor * img.shape[0]) // 2
+        return output[yROI:yROI + intFactor * img.shape[0],
+                      xROI:xROI + intFactor * img.shape[1]]
+    return output
+
+def fht_space_interpolation(image, int magnification=2, bint doMirrorPadding=False):
+    """
+    Fourier interpolation of 2D images or 3D stacks
+    
+    Parameters:
+        image: 2D or 3D numpy array
+        magnification: Integer interpolation factor
+        doMirrorPadding: If True, applies mirror padding before FFT
+    """
+    cdef float[:, :] img2d
+    if image.ndim == 2:
+        img2d = np.ascontiguousarray(image, dtype=np.float32)
+        return np.asarray(_fht_space_interpolation_c(img2d, magnification, doMirrorPadding))
+    elif image.ndim == 3:
+        return np.stack([np.asarray(_fht_space_interpolation_c(
+            np.ascontiguousarray(frame, dtype=np.float32), 
+            magnification, doMirrorPadding)) for frame in image])
+    else:
+        raise ValueError("Input must be 2D or 3D array")
